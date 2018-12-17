f8cad97cd3dc2235e0dcccdf498583a683063b09
Mathias Longo <matlongo@gmail.com>
2018-12-17 10:19:58 -0300

    m

---
+++ AudienceCrossDevicer/run-spark.sh
@@ -0,0 +1,11 @@
+spark-submit\
+  --class "AudienceCrossDevicer"\
+  --master yarn\
+  --deploy-mode cluster\
+  --driver-memory 8g\
+  --executor-memory 8g\
+  --num-executors 10\
+  --executor-cores 3\
+  --queue spark\
+  --conf spark.yarn.maxAppAttempts=1\
+  /home/rely/spark-projects/AudienceCrossDevicer/target/scala-2.11/audience-cross-devicer_2.11-1.0.jar "index_type = 'c'" danone_35936
