7056678fd22f28621838aa09f4a28f42d936d543
Mathias Longo <matlongo@gmail.com>
2018-12-17 18:58:01 -0300

    m

---
+++ CrossDevicer/run-spark.sh
@@ -0,0 +1,11 @@
+spark-submit\
+  --class "CrossDevicer"\
+  --master yarn\
+  --deploy-mode cluster\
+  --driver-memory 8g\
+  --executor-memory 8g\
+  --num-executors 10\
+  --executor-cores 3\
+  --queue default\
+  --conf spark.yarn.maxAppAttempts=1\
+  /home/rely/spark-projects/DataCustomAudiences/target/scala-2.11/data-for-custom-audiences_2.11-1.0.jar 8
