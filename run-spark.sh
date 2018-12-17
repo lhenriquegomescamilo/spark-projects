46822f5d4f4f79d90e545767a7b0b7694c6b5fa4
Mathias Longo <matlongo@gmail.com>
2018-12-17 09:04:16 -0300

    m

--- run-spark.sh
+++ run-spark.sh
@@ -1,5 +1,5 @@
 spark-submit\
-  --class "Random"\
+  --class "AudienceCrossDevicer"\
   --master yarn\
   --deploy-mode cluster\
   --driver-memory 8g\
@@ -8,4 +8,4 @@ spark-submit\
   --executor-cores 3\
   --queue spark\
   --conf spark.yarn.maxAppAttempts=1\
-  /home/rely/spark-projects/Random/target/scala-2.11/random_2.11-1.0.jar 
+  /home/rely/spark-projects/AudienceCrossDevicer/target/scala-2.11/audience-cross-devicer_2.11-1.0.jar
