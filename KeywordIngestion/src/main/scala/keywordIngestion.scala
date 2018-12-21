155cd467801aa65301ade93d68f653ea062c0876
Martin Caravario <mcaravario@dc.uba.ar>
2018-12-21 13:37:16 -0300

    Added download data for elastic functionality

--- KeywordIngestion/src/main/scala/keywordIngestion.scala
+++ KeywordIngestion/src/main/scala/keywordIngestion.scala
@@ -20,21 +20,21 @@ object keywordIngestion {
 
         val daysCount = Days.daysBetween(start, end).getDays()
         val days = (0 until daysCount).map(start.plusDays(_)).map(_.toString(format))
+        val today = DateTime.now().toString("yyyyMMdd")
 
         val dfs = (0 until daysCount).map(start.plusDays(_))
                                      .map(_.toString(format))
-                                     .filter(day => fs.exists(new Path("/datascience/keyword_ingestion/%s.csv".format(day))))
-                                     .map(x => spark.read.format("csv").load("/datascience/keyword_ingestion/%s.csv".format(x)))
+                                     .filter(day => fs.exists(new Path("/datascience/data_keyword_ingestion/%s.csv".format(day))))
+                                     .map(x => spark.read.format("csv").load("/datascience/data_keyword_ingestion/%s.csv".format(x)))
 
         val df = dfs.reduce(_ union _).withColumnRenamed("_c0", "url")
                                       .withColumnRenamed("_c1", "content_keys")
                                       .withColumnRenamed("_c2", "count")
                                       .withColumnRenamed("_c3", "country")
+                                      .drop("country")
 
         // Levantamos el dataframe que tiene toda la data de los usuarios con sus urls
         val udfFilter = udf((segments: Seq[String]) => segments.filter(token => !(token.matches("\\d*"))))
-
-        val today = DateTime.now().toString("yyyyMMdd")
 
         val df_audiences = spark.read.parquet("/datascience/data_audiences_p/day=%s".format(today))
                                       .select("device_id","event_type","all_segments","url","device_type","country")
@@ -46,4 +46,25 @@ object keywordIngestion {
         // Hacemos el join entre nuestra data y la data de las urls con keywords.
         val joint = df_audiences.join(broadcast(df),Seq("url"),"left_outer")
         // Guardamos la data en formato parquet
-        joint.write.format("parquet").mode(SaveMode.Overwrite).save("/datascience/keyword_ingestion_p/%s".format(today))
+        joint.write.format("parquet").mode(SaveMode.Overwrite).save("/datascience/data_keywords_p/%s".format(today))
+
+        // Armamos el archivo que se utilizara para ingestar en elastic
+        val udfAs = udf((segments: Seq[String]) => segments.map(token => "as_"+token))
+        val udfCountry = udf((country: String) =>  "c_"+country)
+        val udfXp = udf((segments: Seq[String], et:String) => if (et == "xp") segments :+ "xp"
+                                                                else segments)
+        val udfJoin = udf((L: Seq[String]) => L.reduce((seg1, seg2) => seg1+","+seg2))
+
+        val to_csv = joint.select("device_id","url_keys","content_keys","all_segments","event_type","country")
+                          .withColumn("all_segments", udfAs($"all_segments"))
+                          .withColumn("all_segments",udfXp($"all_segments",$"event_type"))
+                          .withColumn("country",udfCountry($"country"))
+                          .withColumn("all_segments",udfJoin($"all_segments"))
+                          .withColumn("url_keys",udfJoin($"url_keys"))
+                          .select("device_id","url_keys","content_keys","all_segments","country")
+
+        to_csv.repartition(100)
+              .write.mode(SaveMode.Overwrite)
+              .format("csv")
+              .option("sep", "\t")
+              .save("/datascience/data_keywords_elastic/%s".format(today))
