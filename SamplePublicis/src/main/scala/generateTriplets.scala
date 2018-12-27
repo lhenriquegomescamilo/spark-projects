4077854c6873197cbb7b2ced4a2a065de195432f
Martin Caravario <mcaravario@dc.uba.ar>
2018-12-27 20:47:41 -0300

    Added data publicis processes

---
+++ SamplePublicis/src/main/scala/generateTriplets.scala
@@ -0,0 +1,42 @@
+package main.scala
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{explode,desc,lit,size,concat}
+import org.apache.spark.sql.SaveMode
+import org.joda.time.Days
+import org.joda.time.DateTime
+import org.apache.spark.sql.functions.broadcast
+import org.apache.hadoop.fs.Path
+
+object generateTriplets {
+    def main(args: Array[String]) {
+        /// Configuracion spark
+        val spark = SparkSession.builder.appName("keyword ingestion").getOrCreate()
+        val conf = spark.sparkContext.hadoopConfiguration
+        val fs = org.apache.hadoop.fs.FileSystem.get(conf)
+
+        ////////////////////// ACTUAL EXECUTION ////////////////////////
+
+        /// Este job se encarga de armar triplets de la pinta < dev_id, day, segment > ordenados por fecha y sin duplicados (segment y device_id
+        val format = "yyyyMMdd"
+        val start = DateTime.now.minusDays(30)
+        val end   = DateTime.now.minusDays(0)
+
+        val daysCount = Days.daysBetween(start, end).getDays()
+        val days = (0 until daysCount).map(start.plusDays(_)).map(_.toString(format
+
+        val dfs = (0 until daysCount).map(start.plusDays(_))
+                                      .map(_.toString(format)).reverse
+                                      .filter(day => fs.exists(new org.apache.hadoop.fs.Path("/datascience/data_audiences_p/day=%s".format(day))))
+                                      .map(x => spark.read.parquet("/datascience/data_audiences_p/day=%s".format(x))
+                                          .filter(col("country")==="MX")
+                                          .withColumn("day",lit(x))
+                                          .select("device_id","day","segments"))
+
+        val df = dfs.reduce(_ union _)
+
+        val triplets = df.withColumn("segments",explode(col("segments"))).dropDuplicates(Seq("device_id", "segments"))
+
+        triplets.write.format("csv").mode(SaveMode.Overwrite).save("/datascience/data_publicis_organica_triplets")
+
+    }
+  }
