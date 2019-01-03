5c9ac756f3b5b9cd06cfbf20941ea0b4e8e409ed
Mathias Longo <matlongo@gmail.com>
2019-01-03 14:54:34 -0300

    Added run spark for data GCBA

---
+++ DataGcba/run-spark.sh
@@ -0,0 +1,11 @@
+spark-submit\
+  --class "main.scala.dataGCBA"\
+  --master yarn\
+  --deploy-mode cluster\
+  --driver-memory 8g\
+  --executor-memory 8g\
+  --num-executors 10\
+  --executor-cores 3\
+  --queue spark\
+  --conf spark.yarn.maxAppAttempts=1\
+  /home/rely/spark-projects/DataGcba/target/scala-2.11/data-gcba_2.11-1.0.jar
