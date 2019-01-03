5e9ad9f373a60b8ad8f91b085ce363a4cc5538f9
Mathias Longo <matlongo@gmail.com>
2019-01-03 17:00:52 -0300

    Added sha256 encription to devices_id

--- SamplePublicis/src/main/scala/generateSample.scala
+++ SamplePublicis/src/main/scala/generateSample.scala
@@ -1,6 +1,6 @@
 package main.scala
 import org.apache.spark.sql.{SparkSession, Row, SaveMode}
-import org.apache.spark.sql.functions.{explode,desc,lit,size,concat,col,concat_ws,collect_list,udf,broadcast}
+import org.apache.spark.sql.functions.{explode,desc,lit,size,concat,col,concat_ws,collect_list,udf,broadcast,sha2}
 import org.joda.time.{Days,DateTime}
 import org.apache.hadoop.fs.Path
 
