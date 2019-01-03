62df0534f5b80c50550ebaf5cad12dd446849f6b
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-01-03 12:58:02 -0800

    Random script

--- CrossDevicer/run-spark.sh
+++ CrossDevicer/run-spark.sh
@@ -18,8 +18,8 @@ spark-submit\
   --deploy-mode cluster\
   --driver-memory 8g\
   --executor-memory 8g\
-  --num-executors 15\
-  --executor-cores 4\
-  --queue default\
+  --num-executors 10\
+  --executor-cores 3\
+  --queue spark\
   --conf spark.yarn.maxAppAttempts=1\
   /home/rely/spark-projects/CrossDevicer/target/scala-2.11/cross-devicer_2.11-1.0.jar
