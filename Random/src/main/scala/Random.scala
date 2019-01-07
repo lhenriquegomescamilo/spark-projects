c37531445d88f504447a890e1b561706e6e8f8a8
Mathias Longo <matlongo@gmail.com>
2019-01-07 13:58:59 -0300

    Added process for getting matching estid and device_id

--- Random/src/main/scala/Random.scala
+++ Random/src/main/scala/Random.scala
@@ -1,5 +1,7 @@
 package main.scala
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession, Row, SaveMode}
+import org.apache.spark.sql.functions.{explode,desc,lit,size,concat,col,concat_ws,collect_list,udf,broadcast,sha2}
+import org.joda.time.{Days,DateTime}
 import org.apache.spark.sql.functions.{col, count}
 import org.apache.spark.sql.SaveMode
 
@@ -10,6 +12,7 @@ import org.apache.spark.sql.SaveMode
  */
 object Random {
   def main(args: Array[String]) {
+    val spark = SparkSession.builder.appName("Run matching estid-device_id").getOrCreate()
     val format = "yyyy/MM/dd"
     val start = DateTime.now.minusDays(15)
     val end   = DateTime.now.minusDays(0)
@@ -21,7 +24,7 @@ object Random {
       .map(_.toString(format))
       .map(x => spark.read.format("csv").option("sep", "\t")
                       .option("header", "true")
-                      .load("/data/eventqueue/%s/*.tsv.gz".format(today)))
+                      .load("/data/eventqueue/%s/*.tsv.gz".format(x)))
 
     val df = dfs.reduce(_ union _).filter("d17 is not null and country = 'US'")
                                   .select("d17","device_id")
