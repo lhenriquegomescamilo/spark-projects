029d15ab0f4c0dbefc1aeb4040597733be538a1e
Mathias Longo <matlongo@gmail.com>
2019-01-09 11:49:40 -0300

    Fix keyword ingestion

--- KeywordIngestion/src/main/scala/keywordIngestion.scala
+++ KeywordIngestion/src/main/scala/keywordIngestion.scala
@@ -82,8 +82,8 @@ object keywordIngestion {
       /// Configuracion spark
       val spark = SparkSession.builder.appName("keyword ingestion").getOrCreate()
       val ndays = if (args.length > 0) args(0).toInt else 10
-      //val today = DateTime.now().minusDays(1).toString("yyyyMMdd")
-
+      val today = DateTime.now().minusDays(1).toString("yyyyMMdd")
+      /**
       val format = "yyyyMMdd"
       val start = DateTime.now.minusDays(30)
       val end   = DateTime.now.minusDays(1)
@@ -94,7 +94,8 @@ object keywordIngestion {
       for(day <- days){
         get_data_for_queries(spark,ndays,day)
       }
-      //get_data_for_queries(spark,ndays,today)
+      */
+      get_data_for_queries(spark,ndays,today)
       //get_data_for_elastic(spark,today)
     }
   }
