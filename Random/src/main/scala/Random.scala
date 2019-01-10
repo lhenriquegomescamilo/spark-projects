4169c5c015b9adcb4d87776e0cdd4a2a66898f85
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-01-10 11:36:46 -0800

    random santi pedido

--- Random/src/main/scala/Random.scala
+++ Random/src/main/scala/Random.scala
@@ -28,6 +28,7 @@ object Random {
     val db_index = spark.read.format("parquet")
                              .load("/datascience/crossdevice/double_index/")
                              .filter("device_type IN ('a', 'i')")
+                             .withColumnRenamed("device", "device_id")
 
     val joint = data_us.join(estid_mapping, Seq("d17"))
                        .join(db_index, Seq("device_id"))
