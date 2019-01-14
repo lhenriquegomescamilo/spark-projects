9756a64e90d1a0228d8d89675ccc036e7aa254fb
jif245 <31998143+jif245@users.noreply.github.com>
2019-01-14 17:13:03 -0300

    Update HomeJobs.scala

--- HomeJobs/src/main/scala/HomeJobs.scala
+++ HomeJobs/src/main/scala/HomeJobs.scala
@@ -77,7 +77,7 @@ object HomeJobs {
     //val output_file = "/datascience/geo/MX/specific_POIs"
 
 
-    get_homejobs(spark, safegraph_days, country, output_file)
+    get_homejobs(spark, safegraph_days, country, HourFrom, HourTo, output_file)
   }
 }
 
