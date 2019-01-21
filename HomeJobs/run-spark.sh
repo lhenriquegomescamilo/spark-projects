e78f1fd5a0929aa4ed3129f9779408906162e3a9
JulianFerreiro <jif245@nyu.edu>
2019-01-21 16:44:42 -0300

    added runspark

--- HomeJobs/run-spark.sh
+++ HomeJobs/run-spark.sh
@@ -1,2 +1,12 @@
- spark-submit  --class "main.scala.HomeJobs"  --master yarn  --deploy-mode cluster  --driver-memory 8g  --executor-memory 8g  --num-executors 10  --executor-cores 3  --queue spark  --conf spark.yarn.maxAppAttempts=1  "/home/rely/spark-projects/HomeJobs/target/scala-2.11/homejobs-variable_2.11-1.0.jar"  --nDays 2   --country mexico --HourFrom 20 --HourTo 8 --output /datascience/geo/MX/homes_test
+spark-submit\
+  --class "main.scala.HomeJobs"\
+  --master yarn\
+  --deploy-mode cluster\
+  --driver-memory 8g\
+  --executor-memory 8g\
+  --num-executors 10\
+  --executor-cores 3\
+  --queue spark\
+  --conf spark.yarn.maxAppAttempts=1\
+  "/home/rely/spark-projects/HomeJobs/target/scala-2.11/homejobs-variable_2.11-1.0.jar"  --HourFrom 7 --HourTo 19 --UseType home --country mexico --nDays 2 --output  /datascience/geo/MX/test
 
