c42a7d1495e9706ff51f06d5c335b6b21a41476e
JulianFerreiro <jif245@nyu.edu>
2019-01-21 15:08:59 -0300

    added parameteres. changed filter function

--- HomeJobs/src/main/scala/HomeJobs.scala
+++ HomeJobs/src/main/scala/HomeJobs.scala
@@ -3,7 +3,7 @@ package main.scala
 import org.apache.spark.sql.SparkSession
 import org.apache.hadoop.fs.{ FileSystem, Path }
 import org.joda.time.DateTime
-import org.apache.spark.sql.functions.{round, broadcast, col, abs, to_date, to_timestamp, hour, date_format, from_unixtime}
+import org.apache.spark.sql.functions.{round, broadcast, col, abs, to_date, to_timestamp, hour, date_format, from_unixtime,count, avg}
 import org.apache.spark.sql.SaveMode
 
 object HomeJobs {
