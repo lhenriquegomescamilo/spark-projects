db6ca6225958f701aa3cca3020d12ee0097d236f
Martin Caravario <mcaravario@dc.uba.ar>
2019-01-22 10:28:02 -0300

    Added features to Demo classifier

--- Random/src/main/scala/Random.scala
+++ Random/src/main/scala/Random.scala
@@ -164,18 +164,18 @@ object Random {
 
     val grouped_data = indexed_data.groupBy("device_id","label").agg(collect_list("featureIndex").as("features"),collect_list("count").as("counts"))
 
-//    val udfLabeledPoint = udf((label: Int, features: Seq[Double], counts:Seq[Int], maximo:Int) => 
-//                                                LabeledPoint(label, Vectors.sparse(features.length, 
- //                                                                                  features.toList.map(f => f.toInt).toArray, 
-  //                                                                                 counts.toList.map(f => f.toDouble).toArray)))
+    val udfLabeledPoint = udf((label: Int, features: Seq[Double], counts:Seq[Int], maximo:Int) => 
+                                                LabeledPoint(label, Vectors.sparse(features.length, 
+                                                                                  features.toList.map(f => f.toInt).toArray, 
+                                                                                 counts.toList.map(f => f.toDouble).toArray)))
 
     val udfFeatures = udf((label: Int, features: Seq[Double], counts:Seq[Int], maximo:Int) => 
-                                            Vectors.sparse(features.length, 
-                                                            features.toList.map(f => f.toInt).toArray, 
-                                                            counts.toList.map(f => f.toDouble).toArray))
+                                                                Vectors.sparse(features.length, 
+                                                                                features.toList.map(f => f.toInt).toArray, 
+                                                                                counts.toList.map(f => f.toDouble).toArray))
 
     val df_final = grouped_data.withColumn("features", udfFeatures(col("label"), col("features"), col("counts"),lit(maximo)))
-    //withColumn("points", udfLabeledPoint(col("label"), col("features"), col("counts"),lit(maximo)))
+                                .withColumn("labeled_points", udfLabeledPoint(col("label"), col("features"), col("counts"),lit(maximo)))
                                 
     
     df_final.write.mode(SaveMode.Overwrite).save("/datascience/data_demo/labeled_points")
@@ -278,7 +278,7 @@ object Random {
   def main(args: Array[String]) {
     val spark = SparkSession.builder.appName("Run matching estid-device_id").getOrCreate()
     //getTapadIndex(spark)
-    getTapadOverlap(spark)
-    //getTestSet(spark)
+    //getTapadOverlap(spark)
+    getTestSet(spark)
   }
 }
