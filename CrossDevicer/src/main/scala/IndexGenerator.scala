3c456071ad4a30a856a4b2e4b0fcaa699ff87237
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-01-23 15:02:30 -0800

    lookalike modeling

--- CrossDevicer/src/main/scala/IndexGenerator.scala
+++ CrossDevicer/src/main/scala/IndexGenerator.scala
@@ -20,17 +20,17 @@ object IndexGenerator {
     **/
     def generate_index_double(spark: SparkSession) {
         // This is the path to the last DrawBridge id
-        val drawBridgePath = "/data/crossdevice/2018-12-21/*.gz"
+        val drawBridgePath = "/data/crossdevice/2019-01-17/*.gz"
         // First we obtain the data from DrawBridge
         val db_data = spark.read.format("csv").load(drawBridgePath)
         // Now we obtain a dataframe with only two columns: index (the device_id), and the device type
         val index = db_data.withColumn("all_devices", split(col("_c0"), "\\|")) // First we get the list of all devices
                            .withColumn("index", explode(col("all_devices"))) // Now we generate the index column for every device in the list
-                           .withColumn("index_type", col("index").substr(lit(1), lit(1))) // We obtain the device type just checking the first letter
+                           .withColumn("index_type", col("index").substr(lit(1), lit(3))) // We obtain the device type just checking the first 3 letters
                            .withColumn("index", split(col("index"), ":"))  
                            .withColumn("index", col("index").getItem(1)) // Now we obtain the device_id
                            .withColumn("device", explode(col("all_devices"))) // In this part we get the device that have matched
-                           .withColumn("device_type", col("device").substr(lit(1), lit(1))) // We obtain the device type just checking the first letter
+                           .withColumn("device_type", col("device").substr(lit(1), lit(3))) // We obtain the device type just checking the first 3 letters
                            .withColumn("device", split(col("device"), ":"))  
                            .withColumn("device", col("device").getItem(1)) // Now we obtain the device_id
                            .filter("index_type != 'd' AND device_type != 'd'")
@@ -55,7 +55,7 @@ object IndexGenerator {
         // the second one is a list of devices coming from such cookie,
         // the third column is the list of types that corresponds to the devices
         val df = spark.read.format("parquet").load("/datascience/crossdevice/double_index")
-                                          .filter("index_type = 'c'")
+                                          .filter("index_type = 'coo'")
                                           .groupBy("index")
                                           .agg(collect_list("device") as "devices",
                                                collect_list("device_types") as "types")
@@ -63,15 +63,17 @@ object IndexGenerator {
         // This UDF takes two lists: devices and their corresponding types. As a result, it generates a tuple with three lists,
         // (cookies, androids, ios). Where cookies is the list of devices that are of type 'c' (a cookie).
         val udfDevice = udf((devices: Seq[String], types: Seq[String]) => 
-                                                            ((devices zip types).filter(tuple => tuple._2.charAt(0)=='c').map(tuple => tuple._1),
-                                                             (devices zip types).filter(tuple => tuple._2.charAt(0)=='a').map(tuple => tuple._1),
-                                                             (devices zip types).filter(tuple => tuple._2.charAt(0)=='i').map(tuple => tuple._1)))
+                                                            ((devices zip types).filter(tuple => tuple._2.substring(0, 3)=="coo").map(tuple => tuple._1),
+                                                             (devices zip types).filter(tuple => tuple._2.substring(0, 3)=="and").map(tuple => tuple._1),
+                                                             (devices zip types).filter(tuple => tuple._2.substring(0, 3)=="ios").map(tuple => tuple._1),
+                                                             (devices zip types).filter(tuple => tuple._2.substring(0, 3)=="con").map(tuple => tuple._1)))
 
         // Here we obtain the three lists and leave them as separate columns. Then we rename the index column as 'device_id'.
         val index_xd = df.withColumn("devices", udfDevice(col("devices"), col("types")).cache
                          .withColumn("android", col("devices._2"))
                          .withColumn("ios",     col("devices._3"))
                          .withColumn("cookies", col("devices._1"))
+                         .withColumn("tv", col("devices._4"))
                          .withColumnRenamed("index","device_id")
                          .drop("devices")
 
