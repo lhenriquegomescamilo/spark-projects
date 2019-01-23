cb0112f316ead4c031e9eaa3689f5601a5bb3565
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-01-23 08:32:49 -0800

    netquest

--- AudienceCrossDevicer/run-spark.sh
+++ AudienceCrossDevicer/run-spark.sh
@@ -4,8 +4,8 @@ spark-submit\
   --deploy-mode cluster\
   --driver-memory 8g\
   --executor-memory 8g\
-  --num-executors 15\
+  --num-executors 10\
   --executor-cores 4\
   --queue default\
   --conf spark.yarn.maxAppAttempts=1\
-  /home/rely/spark-projects/AudienceCrossDevicer/target/scala-2.11/audience-cross-devicer_2.11-1.0.jar "index_type = 'c' AND device_type IN ('a', 'i')" /datascience/audiences/output/entel_cl
+  /home/rely/spark-projects/AudienceCrossDevicer/target/scala-2.11/audience-cross-devicer_2.11-1.0.jar --filter "index_type IN ('a', 'i')" --sep "\t" --column "_c0" /datascience/geo/AR/publicidad_sarmiento_30_22-01/
