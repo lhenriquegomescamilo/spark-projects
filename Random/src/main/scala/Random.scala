ee46aee6c3356cad55f2f4a07563fb25e59f4ae4
jif245 <31998143+jif245@users.noreply.github.com>
2019-01-24 13:06:36 -0300

    Update Random.scala

--- Random/src/main/scala/Random.scala
+++ Random/src/main/scala/Random.scala
@@ -1,6 +1,6 @@
 package main.scala
 import org.apache.spark.sql.{SparkSession, Row, SaveMode}
-import org.apache.spark.sql.functions.{explode, desc, lit, size, concat, col, concat_ws, collect_list, udf, broadcast, upper, to_date, to_timestamp, hour, date_format, sha2, count, max, avg, min, split,sum, abs}
+import org.apache.spark.sql.functions.-
 import org.joda.time.{Days,DateTime}
 import org.apache.hadoop.fs.{ FileSystem, Path }
 import org.apache.spark.sql.{ SaveMode, DataFrame }
