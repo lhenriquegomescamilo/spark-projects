3b4dbcbb995b355b5eba90133c89aa4d7067b7ef
jif245 <31998143+jif245@users.noreply.github.com>
2019-01-25 18:20:32 -0300

    Update run-spark.sh

--- POICrossDevicer/run-spark.sh
+++ POICrossDevicer/run-spark.sh
@@ -6,6 +6,6 @@ spark-submit\
   --executor-memory 8g\
   --num-executors 10\
   --executor-cores 3\
-  --queue spark\
+  --queue default\
   --conf spark.yarn.maxAppAttempts=1\
   "/home/rely/spark-projects/POI matcher/target/scala-2.11/poi-matcher_2.11-1.0.jar"  --nDays 2 --poi_file hdfs://rely-hdfs/datascience/geo/GCBAPoisSeguridad.csv --country argentina --output /datascience/geo/MX/specific_POIs2
