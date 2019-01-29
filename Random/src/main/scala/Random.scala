dd7b53e491766216da302fe1472fa35e9654806d
Martin Caravario <mcaravario@dc.uba.ar>
2019-01-29 17:13:03 -0300

    Fix metrics classifier

--- Random/src/main/scala/Random.scala
+++ Random/src/main/scala/Random.scala
@@ -439,7 +439,7 @@ object Random {
     val predictionLabelsRDD = predictions
       .select("predicted_label", "label")
       .rdd
-      .map(r => (r(0).toInt.toDouble, r(1).toInt.toDouble))
+      .map(r => (r.getInt(0).toDouble, r.getInt(1).toDouble))
     val binMetrics = new BinaryClassificationMetrics(predictionLabelsRDD)
 
     val auc = binMetrics.areaUnderROC
