a6d1e2c43ce8799331d52cf4812736e13d3b9677
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-01-29 10:42:57 -0800

    tapad

--- Random/src/main/scala/Random.scala
+++ Random/src/main/scala/Random.scala
@@ -652,7 +652,7 @@ object Random {
     // This file contains 10 columns: "device_id", "device_type","country","id_partner","data_type","ml_sh2", "mb_sh2", "nid_sh2","day"
     val piiData = spark.read
       .format("parquet")
-      .load("datascience/pii_matching/pii_tuples")
+      .load("/datascience/pii_matching/pii_tuples")
       .select("device_id", "ml_sh2")
       .filter("ml_sh2 IS NOT NULL and length(ml_sh2)>0")
       .distinct()
