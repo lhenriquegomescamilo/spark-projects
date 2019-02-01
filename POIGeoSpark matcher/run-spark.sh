d14fba152053efbe1b3a8821d82ac6d13b1e0a4f
Julian <jif245@nyu.edu>
2019-02-01 17:48:22 -0300

    added test for geospark

--- POIGeoSpark matcher/run-spark.sh
+++ POIGeoSpark matcher/run-spark.sh
@@ -8,5 +8,6 @@ spark-submit\
   --executor-cores 3\
   --queue default\
   --conf spark.yarn.maxAppAttempts=1\
+  --packages org.datasyslab:geospark:1.1.0,org.datasyslab:geospark-viz:1.1.0
   "/home/rely/spark-projects/POI matcher/target/scala-2.11/poi-matcher_2.11-1.0.jar"  --nDays 10 --poi_file "hdfs://rely-hdfs/datascience/geo/MX/airports_mx.csv" --country mexico --output /datascience/geo/MX/specific_POIs2
    --country mexico --output /datascience/geo/MX/airports_mx_geospark_10
