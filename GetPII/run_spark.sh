d65a71df958536fd9c20acf7ef6ac7467d255ced
Mathias Longo <matlongo@gmail.com>
2019-02-06 16:24:15 +0000

    segmetn_exlusion tsv

--- GetPII/run_spark.sh
+++ GetPII/run_spark.sh
@@ -1,10 +1,11 @@
 spark-submit\
   --master yarn\
+  --class "main.scala.FromEventqueuePII"\
   --deploy-mode cluster\
   --driver-memory 8g\
   --executor-memory 8g\
-  --num-executors 15\
-  --executor-cores 5\
+  --num-executors 12\
+  --executor-cores 3\
   --queue default\
   --conf spark.yarn.maxAppAttempts=1\
-  /home/rely/spark-projects/GetPII/target/scala-2.11/random_2.11-1.0.jar
+  /home/rely/spark-projects/GetPII/target/scala-2.11/pii-table_2.11-1.0.jar
