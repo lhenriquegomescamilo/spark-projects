b7ddcb50d06618a5b5d51a2e6ff36cab8b80883b
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-02-07 13:02:02 -0800

    bytwo sample

--- ByTwoGeoData/src/main/scala/ByTwoGeoData.scala
+++ ByTwoGeoData/src/main/scala/ByTwoGeoData.scala
@@ -1,6 +1,7 @@
 package main.scala
 
 import org.joda.time.DateTime
+import org.apache.spark.sql.functions.col
 import org.joda.time.format.DateTimeFormat
 import org.apache.spark.sql.{DataFrame, SparkSession, SaveMode}
 
@@ -18,6 +19,32 @@ object ByTwoGeoData {
       .save("/datascience/custom/geo/US/%s/".format(day))
     println("\nLOGGER: DAY %s HAS BEEN PROCESSED!\n\n".format(day))
   }
+
+
+  def encrypt(value: String): String = {
+    val cipher: Cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+    cipher.init(Cipher.ENCRYPT_MODE, keyToSpec())
+    Base64.encodeBase64String(cipher.doFinal(value.getBytes("UTF-8")))
+  }
+
+  def decrypt(encryptedValue: String): String = {
+    val cipher: Cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING")
+    cipher.init(Cipher.DECRYPT_MODE, keyToSpec())
+    new String(cipher.doFinal(Base64.decodeBase64(encryptedValue)))
+  }
+
+  def keyToSpec(): SecretKeySpec = {
+    var keyBytes: Array[Byte] = (SALT + KEY).getBytes("UTF-8")
+    val sha: MessageDigest = MessageDigest.getInstance("SHA-1")
+    keyBytes = sha.digest(keyBytes)
+    keyBytes = util.Arrays.copyOf(keyBytes, 16)
+    new SecretKeySpec(keyBytes, "AES")
+  }
+
+  private val SALT: String =
+    "jMhKlOuJnM34G6NHkqo9V010GhLAqOpF0BePojHgh1HgNg8^72k"
+
+  private val KEY: String = "a51hgaoqpgh5bcmhyt1zptys=="
 
   def getSampleATT(spark: SparkSession, day: String) = {
     println("LOGGER: processing day %s".format(day))
@@ -48,14 +75,14 @@ object ByTwoGeoData {
   }
 
   def main(Args: Array[String]) {
-    // val spark = SparkSession.builder.appName("ByTwo data").getOrCreate()
+    val spark = SparkSession.builder.appName("ByTwo data").getOrCreate()
 
-    // val format = "yyyyMMdd"
-    // val formatter = DateTimeFormat.forPattern("dd/MM/yyyy")
-    // val start = formatter.parseDateTime("14/11/2018")
-    // val days = (0 until 50).map(n => start.plusDays(n)).map(_.toString(format))
+    val format = "yyyyMMdd"
+    val formatter = DateTimeFormat.forPattern("dd/MM/yyyy")
+    val start = formatter.parseDateTime("14/11/2018")
+    val days = (0 until 50).map(n => start.plusDays(n)).map(_.toString(format))
 
     // days.map(day => sampleSanti(spark, day))
-    getSampleATT(spark)
+    days.map(day => getSampleATT(spark, day))
   }
 }
