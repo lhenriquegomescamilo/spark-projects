0635120f98291afaca9327c5bbb3047175fc5b2d
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-02-07 10:24:35 -0800

    bytwo sample

---
+++ ByTwoGeoData/run-spark.sh
@@ -0,0 +1,11 @@
+spark-submit\
+  --class "main.scala.CrossDevicer"\
+  --master yarn\
+  --deploy-mode cluster\
+  --driver-memory 8g\
+  --executor-memory 8g\
+  --num-executors 15\
+  --executor-cores 4\
+  --queue default\
+  --conf spark.yarn.maxAppAttempts=1\
+  /home/rely/spark-projects/CrossDevicer/target/scala-2.11/cross-devicer_2.11-1.0.jar 
