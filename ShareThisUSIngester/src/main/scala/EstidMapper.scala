f27b4326f16a3b75cbcadc2438f2d74b9e160e95
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-02-14 08:05:40 -0800

    m

--- ShareThisUSIngester/src/main/scala/EstidMapper.scala
+++ ShareThisUSIngester/src/main/scala/EstidMapper.scala
@@ -4,9 +4,10 @@ import org.joda.time.DateTime
 import org.apache.spark.sql.functions.lit
 
 object EstidMapper {
-    /**
-     * This function takes a day as input, and produces a set of tuples, where every tuple has the 
-     * estid, device_id, device_type, and day.
+
+  /**
+    * This function takes a day as input, and produces a set of tuples, where every tuple has the
+    * estid, device_id, device_type, and day.
     */
   def getEstIdsMatching(spark: SparkSession, day: String) = {
     val df = spark.read
@@ -28,18 +29,45 @@ object EstidMapper {
       .save("/datascience/sharethis/estid_table/")
   }
 
-  
+  def crossDeviceTable(spark: SparkSession) = {
+    // Get the estid table
+    val estid_table = spark.read
+      .load("/datascience/sharethis/estid_table/")
+      .withColumn("device_id", upper(col("device_id")))
+
+    // Get DrawBridge Index. Here we transform the device id to upper case too.
+    val db_data = spark.read
+      .format("parquet")
+      .load("/datascience/crossdevice/double_index")
+      .filter(index_filter)
+      .withColumn("index", upper(col("index")))
+      .select("index", "device", "device_type")
+
+    // Here we do the cross-device per se.
+    val cross_deviced = db_data
+      .join(audience, db_data.col("index") === audience.col("device_id"))
+      .select("d17", "device", "device_type")
+      .withColumnRenamed("d17", "estid")
+
+    // Finally, we store the result obtained.
+    val output_path =
+      "/datascience/sharethis/estid_madid_table/"
+    cross_deviced.write.mode(SaveMode.Overwrite).save(output_path)
+  }
+
   def main(args: Array[String]) {
     val spark =
       SparkSession.builder.appName("Run matching estid-device_id").getOrCreate()
-      
-    val today = DateTime.now()
-    val days = (31 until 60).map(
-      days =>
-        getEstIdsMatching(
-          spark,
-          today.minusDays(days).toString("yyyy/MM/dd")
-        )
-    )
+
+    // val today = DateTime.now()
+    // val days = (1 until 1).map(
+    //   days =>
+    //     getEstIdsMatching(
+    //       spark,
+    //       today.minusDays(days).toString("yyyy/MM/dd")
+    //     )
+    // )
+
+    crossDeviceTable(spark)
   }
 }
