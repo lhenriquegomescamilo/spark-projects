a312660e64e4d6b28a3e47a8cc280cec356ddeb4
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-02-22 08:31:17 -0800

    test demo

--- DataDemo/src/main/scala/trainModel.scala
+++ DataDemo/src/main/scala/trainModel.scala
@@ -299,9 +299,9 @@ object TrainModel {
     
     //getTrainingSet(spark,country)
     //train_model(spark,country)
-    getTestSet(spark,country)
+    // getTestSet(spark,country)
     //getLabeledPointTrain(spark,country)
-    //getLabeledPointTest(spark,country)
+    getLabeledPointTest(spark,country)
     // generate_expansion(spark,country)
   }
 
