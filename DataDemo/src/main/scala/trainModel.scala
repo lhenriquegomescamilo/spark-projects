6482ad7b168ef20697c8aa7e8d9603897717064b
Martin Caravario <mcaravario@dc.uba.ar>
2019-03-01 14:02:00 -0300

    Remove dupĺicates from gt sets

--- DataDemo/src/main/scala/trainModel.scala
+++ DataDemo/src/main/scala/trainModel.scala
@@ -282,8 +282,8 @@ object TrainModel {
     val spark = SparkSession.builder.appName("Train and evaluate model").getOrCreate()
     val country = if (args.length > 0) args(0).toString else "MX"
     
-    getTrainingSet(spark,country)
-    //getLabeledPointTrain(spark,country)
+    //getTrainingSet(spark,country)
+    getLabeledPointTrain(spark,country)
     //train_model(spark,country)
     //getTestSet(spark,country)
     //getLabeledPointTest(spark,country)
