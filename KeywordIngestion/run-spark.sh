396b0c7b239a1827db20a99c989456ac6ae586c6
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-03-11 12:47:23 -0700

    m

--- KeywordIngestion/run-spark.sh
+++ KeywordIngestion/run-spark.sh
@@ -10,6 +10,6 @@
 	  --executor-cores 4\
 	  --queue default\
 	  --conf spark.yarn.maxAppAttempts=1\
-          --conf spark.sql.shuffle.partitions=500\
+      --conf spark.sql.shuffle.partitions=500\
 	  /home/rely/spark-projects/KeywordIngestion/target/scala-2.11/keyword-ingestion_2.11-1.0.jar 10 1 1
 
