2242985cae0e01899aa5c92102aaa7a05d8e427b
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-03-15 10:36:45 -0700

    m

--- KeywordIngestion/src/main/scala/keywordIngestion.scala
+++ KeywordIngestion/src/main/scala/keywordIngestion.scala
@@ -202,7 +202,7 @@ object keywordIngestion {
     val flatten = udf((xs: Seq[Seq[String]]) => xs.flatten)
 
     // Primero levantamos el dataframe que tiene toda la data de los usuarios con sus urls
-    val URLkeys = getKeywordsByURL(spark, ndays, today, since)//.repartition(100)
+    val URLkeys = getKeywordsByURL(spark, ndays, today, since).repartition(100)
 
     // Ahora levantamos la data de las audiencias
     val df_audiences = getAudienceData(spark, today)
