46070caa9ece2caff92c125ef359a68113a58cdd
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-04-04 08:21:30 -0700

    geo

--- POIGeoSpark matcher/src/main/scala/POIGeoSParkMatcher.scala
+++ POIGeoSpark matcher/src/main/scala/POIGeoSParkMatcher.scala
@@ -151,7 +151,7 @@ This method reads the safegraph data, selects the columns "ad_id" (device id), "
       """select ad_id,
                 utc_timestamp,
                 price_per_m2,
-                ST_Distance(pointdfdist1.pointshape1, pointdfdist2.pointshape2) as distance  
+                ST_Distance(pointdfdist2.pointshape2, pointdfdist1.pointshape1) as distance  
          from (select * from pointdf1 distribute by pointdf1.pointshape1) as pointdfdist1, (select * from pointdf2 distribute by pointdf2.pointshape2) as pointdfdist2
          where ST_Distance(pointdfdist1.pointshape1, pointdfdist2.pointshape2)  < radius"""
     )
