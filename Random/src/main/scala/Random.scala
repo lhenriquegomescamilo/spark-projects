7a2ff165f801e4348275cdcd31ea39c95122a57b
jif245 <31998143+jif245@users.noreply.github.com>
2019-04-09 16:44:44 -0300

    Update Random.scala

--- Random/src/main/scala/Random.scala
+++ Random/src/main/scala/Random.scala
@@ -1849,7 +1849,8 @@ def get_ISP_users(
 
     // Now we obtain the list of hdfs folders to be read
     val path = "/datascience/data_audiences/"
-    val hdfs_files = days.map(day => path + "day=%s/country=AR/".format(day))
+     val hdfs_files = days.map(day => path + "day=%s/country=AR/*".format(day))
+              .filter(path => fs.exists(new org.apache.hadoop.fs.Path(path)))
         
     //cargamos el df de audiences
     val df_audiences = spark.read.parquet(hdfs_files: _*) 
