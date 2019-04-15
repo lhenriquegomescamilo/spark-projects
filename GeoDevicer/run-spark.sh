3cf306a6e10dd62948066203f7def9a1c1317b93
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-04-14 21:29:43 -0700

    m

--- GeoDevicer/run-spark.sh
+++ GeoDevicer/run-spark.sh
@@ -7,5 +7,9 @@ spark-submit\
   --num-executors 10\
   --executor-cores 3\
   --queue spark\
+  --jars geospark-1.1.3.jar,geospark-sql_2.3-1.1.3.jar,geospark-viz-1.1.3.jar\
+  --conf spark.serializer=org.apache.spark.serializer.KryoSerializer\
+  --conf spark.kryo.registrator=org.datasyslab.geosparkviz.core.Serde.GeoSparkVizKryoRegistrator\
   --conf spark.yarn.maxAppAttempts=1\
+  --conf spark.sql.broadcastTimeout=3000000\
   "/home/rely/spark-projects/GeoDevicer/target/scala-2.11/geodevicer_2.11-1.0.jar" --path_geo_json /datascience/geo/geo_json/ecobici_poi_60d_argentina_14-3-2019-13h.json
