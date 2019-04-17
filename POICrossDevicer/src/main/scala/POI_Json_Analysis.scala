a167b53fec39fa633c81aa946e447186a1073e5c
Julian <jif245@nyu.edu>
2019-04-17 14:48:45 -0300

    fixes

--- POICrossDevicer/src/main/scala/POI_Json_Analysis.scala
+++ POICrossDevicer/src/main/scala/POI_Json_Analysis.scala
@@ -313,8 +313,7 @@ def make_analytics_map(spark: SparkSession, value_dictionary: Map [String,String
                               .agg(collect_list(col("utc_timestamp")).as("times_array"),
                                     collect_list("distance").as("distance_array"))
                               .withColumn("frequency", size(col("times_array")))
-                              .withColumn("time_list", concat_ws(",", col("times_array"))).drop("times_array")
-                              .withColumn("distance_list", concat_ws(",", col("distance_array"))).drop("distance_array")
+                              
                               //.filter("(frequency >1)")
 
                    //creamos una función que nos dice si es usuario o no utilizando las siguientes variables
@@ -333,6 +332,8 @@ def make_analytics_map(spark: SparkSession, value_dictionary: Map [String,String
                   //este es un archivo un poco procesado que nos queremos guardar para tal vez hacer procesamientos posteriores
                   val poi_d = poi_c.join(poi_true_users.select("device_id","true_user"),Seq("device_id"),"outer")
                                   .na.fill(false)
+                                  .withColumn("time_list", concat_ws(",", col("times_array"))).drop("times_array")
+                                  .withColumn("distance_list", concat_ws(",", col("distance_array"))).drop("distance_array")
 
 
                   //tenemos que guardarlo
