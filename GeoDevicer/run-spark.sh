fb5deb00b6e5faa89d9dcfbde14ac4c12e22c030
jif245 <31998143+jif245@users.noreply.github.com>
2019-04-24 16:58:43 -0300

    Update run-spark.sh

--- GeoDevicer/run-spark.sh
+++ GeoDevicer/run-spark.sh
@@ -12,4 +12,4 @@ spark-submit\
   --conf spark.kryo.registrator=org.datasyslab.geosparkviz.core.Serde.GeoSparkVizKryoRegistrator\
   --conf spark.yarn.maxAppAttempts=1\
   --conf spark.sql.broadcastTimeout=3000000\
-  "/home/rely/spark-projects/GeoDevicer/target/scala-2.11/geodevicer_2.11-1.0.jar" --path_geo_json Lat_Long_Sarmiento_91d_argentina_24-4-2019-11h
+  "/home/rely/spark-projects/GeoDevicer/target/scala-2.11/geodevicer_2.11-1.0.jar" --path_geo_json sarmiento_pois_3d_argentina_22-4-2019-11h
