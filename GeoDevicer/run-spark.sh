fa90252187e2006308c86a5d15ecb7cddec54d89
jif245 <31998143+jif245@users.noreply.github.com>
2019-04-24 16:00:31 -0300

    Update run-spark.sh

--- GeoDevicer/run-spark.sh
+++ GeoDevicer/run-spark.sh
@@ -12,4 +12,4 @@ spark-submit\
   --conf spark.kryo.registrator=org.datasyslab.geosparkviz.core.Serde.GeoSparkVizKryoRegistrator\
   --conf spark.yarn.maxAppAttempts=1\
   --conf spark.sql.broadcastTimeout=3000000\
-  "/home/rely/spark-projects/GeoDevicer/target/scala-2.11/geodevicer_2.11-1.0.jar" --path_geo_json /datascience/geo/geo_json/ecobici_poi_60d_argentina_14-3-2019-13h.json
+  "/home/rely/spark-projects/GeoDevicer/target/scala-2.11/geodevicer_2.11-1.0.jar" --path_geo_json /datascience/geo/geo_json/Lat_Long_Sarmiento_90d_argentina_24-4-2019-9h.json
