5f17d555133743616bcd7a327b7895d6262c24ee
Mathias Longo <matlongo@gmail.com>
2019-05-27 17:07:14 -0300

    Reordering scripts

--- DataDemo/src/main/scala/monthly/generateMonthlyFeatures.scala
+++ DataDemo/src/main/scala/monthly/generateMonthlyFeatures.scala
@@ -1,7 +1,6 @@
 package main.scala.monthly
 import main.scala.features.GenerateTriplets
 import main.scala.features.DataGoogleAnalytics
-import main.scala.features.GenerateDataset
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
@@ -13,18 +12,14 @@ import org.apache.hadoop.fs.Path
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.sql.{SaveMode, DataFrame}
 
-object GenerateMonthlyData{
+object GenerateMonthlyFeatures{
   def main(args: Array[String]) {
     val spark = SparkSession.builder
-      .appName("Monthly Data Download")
+      .appName("Monthly Features Download")
       .getOrCreate()
 
     // Parameters
     val ndays = if (args.length > 0) args(0).toInt else 30
-    val country = if (args.length > 1) args(1).toString else ""
-    val training_name = if (args.length > 2) args(2).toString else "training_set"
-    val expansion_name = if (args.length > 3) args(3).toString else "expansion_set"
-    val path_gt = if (args.length > 4) args(4).toString else "/datascience/devicer/processed/AR_xd-0_partner-_pipe-0_2019-04-09T18-18-41-066436_grouped/"
 
     // Path con data del devicer
     val filename_path = "/data/metadata/20190316-paths-counts.tsv"
@@ -37,8 +32,6 @@ object GenerateMonthlyData{
     val ga_domain = DataGoogleAnalytics.get_data_google_analytics(spark, ndays, filename_domain)
     //val ga_path = DataGoogleAnalytics.get_data_google_analytics_path(spark, ndays, filename_path)    
     println("LOGGER: GA data generated")
-    
-    val training = GenerateDataset.getTrainingData(spark, path_gt, country, training_name)
-    val expansion = GenerateDataset.getExpansionData(spark, path_gt, country, expansion_name)
+
   }
 }
