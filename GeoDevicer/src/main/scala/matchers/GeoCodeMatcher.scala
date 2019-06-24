2ba9747d8e07f6df15b05b0097ec31ee35d1982c
Julian <jif245@nyu.edu>
2019-06-24 12:08:15 -0300

    new pipeline

--- GeoDevicer/src/main/scala/matchers/GeoCodeMatcher.scala
+++ GeoDevicer/src/main/scala/matchers/GeoCodeMatcher.scala
@@ -41,7 +41,7 @@ object POICrossDevicerJson {
     val fs = FileSystem.get(conf)
 
     // Get the days to be loaded
-    val format = "yyyy/MM/dd"
+    val format = "yyMMdd"
     val end = DateTime.now.minusDays(value_dictionary("since").toInt)
     val days = (0 until value_dictionary("nDays").toInt)
       .map(end.minusDays(_))
@@ -49,21 +49,20 @@ object POICrossDevicerJson {
       .filter(x => !(x contains "05/27"))
 
     // Now we obtain the list of hdfs files to be read
-    val path = "/data/geo/safegraph/"
+    val path = "/datascience/geo/safegraph_pipeline/" //"day=/country=%s".format(value_dictionary("country"))
     val hdfs_files = days
-      .map(day => path + "%s/".format(day))
+      .map(day => path + "day=0%s/country=%s/".format(day,value_dictionary("country")))
       .filter(
-        path => fs.exists(new org.apache.hadoop.fs.Path("/data/geo/safegraph/"))
+        path => fs.exists(new org.apache.hadoop.fs.Path("/datascience/geo/safegraph_pipeline/"))
       )
-      .map(day => day + "*.gz")
+      .map(day => day + "*.snappy.parquet")
 
     // Finally we read, filter by country, rename the columns and return the data
     val df_safegraph = spark.read
       .option("header", "true")
       .csv(hdfs_files: _*)
       .dropDuplicates("ad_id", "latitude", "longitude")
-      .filter("country = '%s'".format(value_dictionary("country")))
-      .select("ad_id", "id_type", "latitude", "longitude", "utc_timestamp")
+      .select("ad_id", "id_type", "latitude", "longitude", "utc_timestamp") //.filter("country = '%s'".format(value_dictionary("country")))
       .withColumnRenamed("latitude", "latitude_user")
       .withColumnRenamed("longitude", "longitude_user")
       .withColumn(
