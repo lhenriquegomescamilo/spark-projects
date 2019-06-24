d8c5b335c89c5f59ab81f2ae8d4d0b7913dd6bde
Julian <jif245@nyu.edu>
2019-06-24 12:24:31 -0300

    corrections

--- GeoDevicer/src/main/scala/matchers/GeoCodeMatcher.scala
+++ GeoDevicer/src/main/scala/matchers/GeoCodeMatcher.scala
@@ -53,9 +53,12 @@ object POICrossDevicerJson {
     val hdfs_files = days
       .map(day => path + "day=0%s/country=%s/".format(day,value_dictionary("country")))
       .filter(
-        path => fs.exists(new org.apache.hadoop.fs.Path("/datascience/geo/safegraph_pipeline/"))
-      )
-      .map(day => path + "day=0%s/country=%s/*.snappy.parquet".format(day,value_dictionary("country")))
+        path => 
+          fs.exists(new org.apache.hadoop.fs.Path(
+            "/datascience/geo/safegraph_pipeline/"
+            .format(day,value_dictionary("country")))
+              )
+      .map(day => day + "*.snappy.parquet")
 
     // Finally we read, filter by country, rename the columns and return the data
     val df_safegraph = spark.read
