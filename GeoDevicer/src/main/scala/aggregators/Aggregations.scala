7d18fcc381dddad3aacc1a0de230a7e58d7c236a
Julian <jif245@nyu.edu>
2019-07-04 16:41:24 -0300

    polygon

--- GeoDevicer/src/main/scala/aggregators/Aggregations.scala
+++ GeoDevicer/src/main/scala/aggregators/Aggregations.scala
@@ -136,7 +136,9 @@ def userAggregate_fromPolygon(
     val stayedThere = udf( (timestamps: Seq[String]) => 
             (timestamps.slice(1, timestamps.length) zip timestamps).map(
                     t =>
-                ( t._1.toInt-t._2.toInt<umbralMax.toInt * 60) & (t._1.toInt-t._2.toInt>umbralMin.toInt * 60))
+                ( t._1.toInt-t._2.toInt < value_dictionary("umbralMax").toInt * 60) 
+                                  & (
+                  t._1.toInt-t._2.toInt > value_dictionary("umbralMin").toInt * 60))
                     .exists(b=>b) )
     
     // Path where we will store the results.
