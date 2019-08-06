6f51ccae273a87ef43ead16baf7f4d694ce0c177
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-08-06 13:39:28 -0300

    adding a 3rd queue

--- Retroactivations/src/main/scala/devicer/GetAudience.scala
+++ Retroactivations/src/main/scala/devicer/GetAudience.scala
@@ -1129,6 +1129,8 @@ object GetAudience {
 
       case 1 => "/datascience/devicer/priority/"
 
+      case 2 => "/datascience/devicer/queue-03"
+
     }
 
     println("LOGGER: Path: %s".format(path))
