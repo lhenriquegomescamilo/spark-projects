b92964bbe888b11a0cb3294988ac46144447d2e7
Mathias Longo <mathiaslongo@Mathiass-MacBook-Pro.local>
2019-08-06 15:09:44 -0300

    isp in random

--- Random/src/main/scala/Random.scala
+++ Random/src/main/scala/Random.scala
@@ -3439,26 +3439,26 @@ user_granularity.write
   }
 
 
- def get_pii_AR_seba(spark: SparkSession) {
+//  def get_pii_AR_seba(spark: SparkSession) {
     
-   val piis_ar = spark.read.format("parquet")
-    .load("/datascience/pii_matching/pii_tuples/day=20190731/")
-    .filter("country == 'AR'")
-    .select("device_id","ml_sh2","mb_sh2","nid_sh2")
-    .filter((col("ml_sh2").isNotNull) or (col("mb_sh2").isNotNull) or (col("nid_sh2").isNotNull))
-    .dropDuplicates() 
+//    val piis_ar = spark.read.format("parquet")
+//     .load("/datascience/pii_matching/pii_tuples/day=20190731/")
+//     .filter("country == 'AR'")
+//     .select("device_id","ml_sh2","mb_sh2","nid_sh2")
+//     .filter((col("ml_sh2").isNotNull) or (col("mb_sh2").isNotNull) or (col("nid_sh2").isNotNull))
+//     .dropDuplicates() 
     
-    piis_ar.write.format("csv").mode(SaveMode.Overwrite)
-        .save("/datascience/misc/ar_pii_seba")
+//     piis_ar.write.format("csv").mode(SaveMode.Overwrite)
+//         .save("/datascience/misc/ar_pii_seba")
 
-    ar_pii.write
-      .format("csv")
-      .mode(SaveMode.Overwrite)
-      .save("/datascience/misc/ar_pii_seba")
+//     ar_pii.write
+//       .format("csv")
+//       .mode(SaveMode.Overwrite)
+//       .save("/datascience/misc/ar_pii_seba")
 
-    ar_pii.describe().filter(col("summary") === "count").show()
+//     ar_pii.describe().filter(col("summary") === "count").show()
 
-  }
+//   }
 
   /**
     *
