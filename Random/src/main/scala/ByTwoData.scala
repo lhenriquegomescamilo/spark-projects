253e43dabb972694538d8ab0d6965750ae7df1b5
Mathias Longo <mathiaslongo@Mathiass-MBP.utopia.net>
2019-08-07 09:58:28 -0300

    adding bytwo script

---
+++ Random/src/main/scala/ByTwoData.scala
@@ -0,0 +1,121 @@
+package main.scala
+import org.apache.spark.sql.{SparkSession, Row, SaveMode}
+import org.apache.spark.sql.functions._
+import org.joda.time.{Days, DateTime}
+import org.joda.time.format.DateTimeFormat
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.sql.{SaveMode, DataFrame}
+import org.apache.spark.ml.attribute.Attribute
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer}
+//import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Encoders, SparkSession}
+import org.joda.time.Days
+import org.joda.time.DateTime
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.ml.classification.{
+  RandomForestClassificationModel,
+  RandomForestClassifier
+}
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.classification.{
+  GBTClassificationModel,
+  GBTClassifier
+}
+
+/**
+  * The idea of this script is to run random stuff. Most of the times, the idea is
+  * to run quick fixes, or tests.
+  */
+object Random {
+
+  def processDay(spark: SparkSession): DataFrame = {
+    println("LOGGER: processing day %s".format(day))
+
+    // List of columns present in the json files
+    val columns = List(
+      "estid",
+      "standardTimestamp",
+      "url",
+      "ip",
+      "deviceType",
+      "os",
+      "browserFamily",
+      "de_geo_isp_name",
+      "de_geo_pulseplus_conn_type",
+      "de_geo_pulseplus_latitude",
+      "de_geo_pulseplus_longitude",
+      "de_geo_pulseplus_city_code",
+      "de_geo_pulseplus_city",
+      "android_id",
+      "ios_idfa",
+      "connected_tv"
+    )
+
+    // Here we process the data
+    spark.read
+      .format("com.databricks.spark.csv")
+      .load("/datascience/sharethis/loading/%s*.json".format(day))
+      // Renaming the columns
+      .withColumnRenamed("_c0", "estid")
+      .withColumnRenamed("_c1", "standardTimestamp")
+      .withColumnRenamed("_c2", "url")
+      .withColumnRenamed("_c3", "ip")
+      .withColumnRenamed("_c4", "deviceType")
+      .withColumnRenamed("_c5", "os")
+      .withColumnRenamed("_c6", "browserFamily")
+      .withColumnRenamed("_c7", "de_geo_isp_name")
+      .withColumnRenamed("_c8", "de_geo_pulseplus_conn_type")
+      .withColumnRenamed("_c9", "de_geo_pulseplus_latitude")
+      .withColumnRenamed("_c10", "de_geo_pulseplus_longitude")
+      .withColumnRenamed("_c11", "de_geo_pulseplus_city_code")
+      .withColumnRenamed("_c12", "de_geo_pulseplus_city")
+      .withColumnRenamed("_c13", "android_id")
+      .withColumnRenamed("_c14", "ios_idfa")
+      .withColumnRenamed("_c15", "connected_tv")
+      // Modifying the types
+      .withColumn("ip", col("ip").cast("int"))
+      // Dropping unnecessary columns
+      .drop()
+      .coalesce(100)
+      .write
+      .mode(SaveMode.Overwrite)
+      .format("csv")
+      .option("sep", "\t")
+      .save("/datascience/sharethis/bytwo/day=%s".format(day))
+    println("LOGGER: day %s processed successfully!".format(day))
+  }
+
+  def getDataAudiences(
+      spark: SparkSession,
+      nDays: Int = 30,
+      since: Int = 1
+  ): DataFrame = {
+    // First we obtain the configuration to be allowed to watch if a file exists or not
+    val conf = spark.sparkContext.hadoopConfiguration
+    val fs = FileSystem.get(conf)
+
+    // Get the days to be loaded
+    val format = "yyyyMMdd"
+    val end = DateTime.now.minusDays(since)
+    val days = (0 until nDays).map(end.minusDays(_)).map(_.toString(format))
+    val path = "/datascience/sharethis/loading"
+
+  }
+
+  def main(args: Array[String]) {
+    val spark =
+      SparkSession.builder.appName("Run matching estid-device_id").getOrCreate()
+
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+  }
+
+}
