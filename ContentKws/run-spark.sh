23c9ab36de4fc405ab01abb60126f14d8d3c51bc
Leo <leopoldo.albina@retargetly.com>
2019-08-12 15:33:25 -0300

    msg

--- ContentKws/run-spark.sh
+++ ContentKws/run-spark.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
-
-source /home/rely/.bashrc
-
-/home/rely/spark/bin/spark-submit\
+spark-submit\
   --master yarn\
   --deploy-mode cluster\
   --driver-memory 8g\
   --executor-memory 8g\
-  --num-executors 10\
+  --num-executors 12\
   --executor-cores 3\
   --queue default\
   --conf spark.yarn.maxAppAttempts=1\
-  /home/rely/spark-projects/DataCustomAudiences/target/scala-2.11/data-for-custom-audiences_2.11-1.0.jar --nDays 1 --from 1
+  /home/rely/spark-projects/ContentKws/target/scala-2.11/content-keywords_2.11-1.0.jar
+
+
+
+
