bd9456b5d101b3b3cc8bee094543ca65eacfec14
jif245 <jif245@nyu.edu>
2019-08-27 15:55:56 -0300

    attribution date

--- GeoDevicer/src/main/scala/crossdevicer/CrossDevicer.scala
+++ GeoDevicer/src/main/scala/crossdevicer/CrossDevicer.scala
@@ -72,8 +72,8 @@ object CrossDevicer {
       val cross_deviced = db_data      
       .join(        
         audience    
-        .select("device_id","device_type","validUser","frequency",
-                  value_dictionary("poi_column_name"))      //,     value_dictionary("audience_column_name")    
+        .select("device_id","device_type",
+                  value_dictionary("poi_column_name"))      //,     value_dictionary("audience_column_name"),"validUser","frequency",    
         .distinct(),        
         Seq("device_id"),
             "right_outer")      
