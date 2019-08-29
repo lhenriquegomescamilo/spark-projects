57068a14334915e655b91647c4d8aac11d0419b2
jif245 <jif245@nyu.edu>
2019-08-29 15:57:05 -0300

    el mejor random

--- Random/src/main/scala/elMejorRandom.scala
+++ Random/src/main/scala/elMejorRandom.scala
@@ -12,10 +12,12 @@ import org.apache.hadoop.fs.{FileSystem, Path}
 object elMejorRandom {
   def get_tapad_home_cluster(spark:SparkSession){
 
-val code = spark.read.format("csv").option("header",true).option("delimiter","\t").load("/datascience/geo/argentina_5d_home_14-8-2019-17h").withColumn("device_id",upper(col("ad_id")))
+val code = spark.read.format("csv").option("header",true).option("delimiter","\t")
+.load("/datascience/geo/argentina_365d_home_21-8-2019-0h").withColumn("device_id",upper(col("ad_id")))
 
 
-val poly = spark.read.format("csv").option("header",true).option("delimiter","\t").load("/datascience/geo/radios_argentina_2010_geodevicer_5d_argentina_14-8-2019-17h").withColumn("device_id",upper(col("device_id")))
+val poly = spark.read.format("csv").option("header",true).option("delimiter","\t")
+.load("/datascience/geo/radios_argentina_2010_geodevicer_5d_argentina_14-8-2019-17h").withColumn("device_id",upper(col("device_id")))
 
 val index = spark.read.format("csv").option("delimiter","\t").load("/data/crossdevice/2019-07-14/Retargetly_ids_full_20190714_193703_part_00.gz")
 val home_index = index.withColumn("tmp",split(col("_c2"),"=")).select(col("_c0"),col("tmp").getItem(1).as("_c2")).drop("tmp").filter(col("_c2").isNotNull).toDF("house_cluster","device_id").withColumn("device_id",upper(col("device_id")))
@@ -37,7 +39,57 @@ code.join(home_index,Seq("device_id"))
 
   }
 
+def getDataPipeline(
+      spark: SparkSession,
+      path: String,
+      nDays: String,
+      since: String) = {
+    // First we obtain the configuration to be allowed to watch if a file exists or not
+    val conf = spark.sparkContext.hadoopConfiguration
+    val fs = FileSystem.get(conf)
 
+    //specifying country
+    val country_iso = "AR"
+      /datascience/data_useragents/
+
+        // Get the days to be loaded
+    val format = "yyyyMMdd"
+    val end = DateTime.now.minusDays(since.toInt)
+    val days = (0 until nDays.toInt).map(end.minusDays(_)).map(_.toString(format))
+
+    // Now we obtain the list of hdfs folders to be read
+    val hdfs_files = days
+      .map(day => path + "/day=%s/country=%s".format(day,country_iso))
+      .filter(path => fs.exists(new org.apache.hadoop.fs.Path(path)))
+    val df = spark.read.option("basePath", path).parquet(hdfs_files: _*)
+
+    df
+  }
+
+def get_ua_segments(spark:SparkSession) = {
+
+val ua = spark.read.format("parquet")
+        .load("/datascience/data_useragents/day=*/country=AR")
+        .filter("model != ''") //con esto filtramos los desktop
+        .withColumn("device_id",upper("device_id"))
+        .drop("user_agent","event_type","url")
+        .dropDuplicates("device_id")
+
+val segments = getDataPipeline(spark,"/datascience/data_triplets/segments/",5,10)
+              .withColumn("device_id",upper("device_id"))
+
+val joined = ua.join(df,Seq("device_id"))
+.write.format("csv")
+.option("header",true)
+.option("delimiter","\t")
+.mode(SaveMode.Overwrite)
+.save("/datascience/misc/ua_w_segments_5d")
+
+}
+
+
+
+}
   /*****************************************************/
   /******************     MAIN     *********************/
   /*****************************************************/
@@ -48,7 +100,7 @@ code.join(home_index,Seq("device_id"))
     .getOrCreate()
 
        
-    get_tapad_home_cluster(spark)
+    getDataPipeline(spark)
      
   }
 }
