f38f8ce319d03ac2960cd30ca723abd96db363cf
jif245 <31998143+jif245@users.noreply.github.com>
2019-09-18 16:03:21 -0300

    Update run-spark.sh

--- HomeJobsJr/run-spark.sh
+++ HomeJobsJr/run-spark.sh
@@ -1,15 +1,15 @@
-spark-submit\
-  --class "main.scala.GeoSparkMatcher"\
-  --master yarn\
-  --deploy-mode cluster\
-  --driver-memory 8g\
-  --executor-memory 8g\
-  --num-executors 10\
-  --executor-cores 3\
-  --queue spark\
+spark-submit\  
+  --class 'main.scala.HomeJobsJr'\
+  --master yarn\  
+  --deploy-mode cluster\  
+  --driver-memory 8g\  
+  --executor-memory 8g\  
+  --num-executors 10\  
+  --executor-cores 3\  
+  --queue spark\  
   --jars geospark-1.1.3.jar,geospark-sql_2.3-1.1.3.jar,geospark-viz-1.1.3.jar\
-  --conf spark.serializer=org.apache.spark.serializer.KryoSerializer\
-  --conf spark.kryo.registrator=org.datasyslab.geosparkviz.core.Serde.GeoSparkVizKryoRegistrator\
-  --conf spark.yarn.maxAppAttempts=1\
-  --conf spark.sql.broadcastTimeout=3000000\
-  "/home/rely/spark-projects/GeoDevicer/target/scala-2.11/geodevicer_2.11-1.0.jar" --path_geo_json sarmiento_pois_5d_argentina_2-5-2019-8h
+    --conf spark.serializer=org.apache.spark.serializer.KryoSerializer\  
+    --conf spark.kryo.registrator=org.datasyslab.geosparkviz.core.Serde.GeoSparkVizKryoRegistrator\  
+    --conf spark.yarn.maxAppAttempts=1\  
+    --conf spark.sql.broadcastTimeout=3000000\
+      '/home/rely/spark-projects/HomeJobsJr/target/scala-2.11/homejobsjr_2.11-1.0.jar' --path_geo_json argentina_60d_home_18-9-2019-18h
