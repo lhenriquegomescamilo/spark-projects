9b5ef16e007547cf38cb329ea0744aff18cce588
Elton <lahnlaw@gmail.com>
2019-09-19 15:59:03 -0300

    pii

--- GetPII/src/main/scala/GetPii.scala
+++ GetPII/src/main/scala/GetPii.scala
@@ -52,69 +52,62 @@ object FromEventqueuePII {
 
   def procesPII(spark: SparkSession) {
     // First we load all the data generated from PIIs
-    var data = spark.read.load("/datascience/pii_matching/pii_tuples/")
+    val pii_input = spark.read
+        .format("parquet")
+        .load("/datascience/pii_matching/pii_tuples/")
+        .filter("day >= 20190918")
+        .filter("country in('AR', 'CL', 'PE')")
     // Then we separate the data acording to the PII type
     var mails = data
       .filter("ml_sh2 is not null")
-      .select(
-        "device_id",
-        "device_type",
-        "country",
-        "id_partner",
-        "ml_sh2",
-        "day"
-      )
-      .withColumnRenamed("ml_sh2", "pii")
-      .withColumn("pii_type", lit("mail"))
+      .select("device_id","country")
+      .withColumnRenamed("ml_sh2", "device_id")
+      .withColumn("device_type", lit("mail"))
+      .dropDuplicates("country", "device_id")
+
     var dnis = data
       .filter("nid_sh2 is not null")
-      .select(
-        "device_id",
-        "device_type",
-        "country",
-        "id_partner",
-        "nid_sh2",
-        "day"
-      )
-      .withColumnRenamed("nid_sh2", "pii")
-      .withColumn("pii_type", lit("nid"))
+      .select("device_id","country")
+      .withColumnRenamed("nid_sh2", "device_id")
+      .withColumn("device_type", lit("nid"))
+      .dropDuplicates("country", "device_id")
+
     var mobs = data
       .filter("mb_sh2 is not null")
-      .select(
-        "device_id",
-        "device_type",
-        "country",
-        "id_partner",
-        "mb_sh2",
-        "day"
-      )
-      .withColumnRenamed("mb_sh2", "pii")
-      .withColumn("pii_type", lit("mob"))
-    var total = mails.unionAll(dnis).unionAll(mobs)
+      .select("device_id","country")
+      .withColumnRenamed("mb_sh2", "device_id")
+      .withColumn("device_type", lit("mob"))
+      .dropDuplicates("country", "device_id")
+
+    var total = mails
+        .unionAll(dnis)
+        .unionAll(mobs)
+        .orderBy("country", "device_type", "device_id")
     // We group the data and get the list of pii for each device_id with the correspondant id_partner and timestamp in a tuple.
-    var grouped = total
-      .groupBy("device_id", "country", "pii", "pii_type")
-      .agg(
-        collect_list("id_partner").as("id_partner"),
-        collect_list("day").as("days"),
-        collect_list("device_type").as("device_type")
-      )
+    //var grouped = total
+    //  .groupBy("device_id", "country", "pii", "pii_type")
+    //  .agg(
+    //    collect_list("id_partner").as("id_partner"),
+    //    collect_list("day").as("days"),
+    //    collect_list("device_type").as("device_type")
+    //  )
     // Then we sort the tuples and we keep the one with the smallest timestamp.
     val udfSort = udf(
       (id_partner: Seq[String], days: Seq[String]) =>
         (id_partner zip days).sortBy(_._2).toList(0)
     )
-    val df_final = grouped
-      .withColumn("result", udfSort(col("id_partner"), col("days")))
-      .withColumn("id_partner", col("result").getItem("_1"))
-      .withColumn("days", col("result").getItem("_2"))
-      .select("device_id", "country", "pii", "pii_type", "id_partner", "days")
+    //val df_final = grouped
+    //  .withColumn("result", udfSort(col("id_partner"), col("days")))
+    //  .withColumn("id_partner", col("result").getItem("_1"))
+    //  .withColumn("days", col("result").getItem("_2"))
+    //  .select("device_id", "country", "pii", "pii_type", "id_partner", "days")
 
     // We save the generated file
-    df_final.write
+    total.write
       .format("parquet")
+      .partitionBy("country")
       .mode(SaveMode.Overwrite)
-      .save("/datascience/pii_matching/pii_table")
+      .save("/datascience/pii_matching/temp/")
   }
 
 
@@ -151,7 +144,7 @@ object FromEventqueuePII {
     val days = (0 until nDays).map(end.minusDays(_)).map(_.toString(format))
 
     // Now we effectively download the data day by day
-    days.map(day => getPII(spark, day))
+    //days.map(day => getPII(spark, day))
 
     procesPII(spark)
   }
