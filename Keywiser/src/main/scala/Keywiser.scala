2a868e4ad5f249db4484f1c67e9b09895c6047c9
Leo <leopoldo.albina@retargetly.com>
2019-09-20 17:44:31 -0300

    msg

--- Keywiser/src/main/scala/Keywiser.scala
+++ Keywiser/src/main/scala/Keywiser.scala
@@ -5,43 +5,11 @@ import org.joda.time.{Days, DateTime}
 import org.joda.time.format.DateTimeFormat
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.sql.{SaveMode, DataFrame}
-import org.apache.spark.ml.attribute.Attribute
-import org.apache.spark.ml.feature.{IndexToString, StringIndexer}
-/**
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
-import org.apache.spark.ml.linalg.Vectors
-import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
-**/
-import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{Encoders, SparkSession}
 import org.joda.time.Days
 import org.joda.time.DateTime
 import org.apache.hadoop.conf.Configuration
-/**
-import org.apache.spark.ml.classification.{
-  RandomForestClassificationModel,
-  RandomForestClassifier
-}
-import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
-import org.apache.spark.ml.classification.{
-  GBTClassificationModel,
-  GBTClassifier
-}
-
-import org.apache.spark.mllib.feature.Stemmer
-**/
-import java.security.MessageDigest
-import java.util
-import javax.crypto.Cipher
-import javax.crypto.spec.SecretKeySpec
-import org.apache.commons.codec.binary.Base64
-import java.time.DateTimeException
-import java.sql.Savepoint
 
 /**
   * The idea of this script is to generate audiences based on keywords obtained from url content. 
