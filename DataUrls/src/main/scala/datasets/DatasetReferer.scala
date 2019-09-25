7155553ff40d41bc7faa1a5611ab667ad182b66c
mcaravario <mcaravario@dc.uba.ar>
2019-09-25 12:01:37 -0300

    Adding preprocess to urls (dataset referer)

--- DataUrls/src/main/scala/datasets/DatasetReferer.scala
+++ DataUrls/src/main/scala/datasets/DatasetReferer.scala
@@ -71,10 +71,10 @@ object DatasetReferer {
     val ndays = if (args.length > 0) args(0).toInt else 10
     val since = if (args.length > 1) args(1).toInt else 1
     val country = if (args.length > 2) args(2).toString else ""
-    val segments = List(129, 59, 61, 250, 396, 150, 26, 32, 247, 3013, 3017)
-
     
+    val data_urls = UrlUtils.get_data_urls(spark, ndays, since, country)
     val gtDF = spark.read.load("/datascience/data_url_classifier/gt/country=AR/")
-    get_url_referer(spark, country = country, since = since, ndays = ndays, gtDF = gtDF, joinType = "inner", df_urls = data_urls, name = name = "dataset_referer_training")
+    get_url_referer(spark, country = country, since = since, ndays = ndays, gtDF = gtDF, joinType = "inner", df_urls = data_urls,
+                    name = "dataset_referer_training")
   }
 }
