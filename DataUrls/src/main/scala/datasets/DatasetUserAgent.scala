ac8893d829684dffd3593cb6fea50821913dad82
mcaravario <mcaravario@dc.uba.ar>
2019-09-26 16:34:55 -0300

    fix

--- DataUrls/src/main/scala/datasets/DatasetUserAgent.scala
+++ DataUrls/src/main/scala/datasets/DatasetUserAgent.scala
@@ -129,10 +129,9 @@ object DatasetUserAgent {
       .getOrCreate()
 
     // Parseo de parametros
-    val ndays = if (args.length > 0) args(0).toInt else 10
-    val since = if (args.length > 1) args(1).toInt else 1
-    val country = if (args.length > 2) args(2).toString else ""
-    val segments = List(129, 59, 61, 250, 396, 150, 26, 32, 247, 3013, 3017)
+    val ndays =  10
+    val since =  1
+    val country = "AR"
 
     val untagged_df = UrlUtils.get_data_untagged(spark,ndays,since,country)
     get_url_user_agent(spark, country = country, since = since, ndays = 30, gtDF = untagged_df, joinType = "inner",name="dataset_user_agent_expansion")
