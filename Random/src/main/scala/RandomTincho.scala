b48f8e51d238b8436e1e0250d4346aaa3ae22d9a
mcaravario <mcaravario@dc.uba.ar>
2019-10-02 17:47:53 -0300

    Remove logging warning

--- Random/src/main/scala/RandomTincho.scala
+++ Random/src/main/scala/RandomTincho.scala
@@ -24,6 +24,7 @@ import org.apache.spark.sql.functions.{
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.sql.{SaveMode, DataFrame}
+import org.apache.log4j.{Level, Logger}
 
 object RandomTincho {
 
