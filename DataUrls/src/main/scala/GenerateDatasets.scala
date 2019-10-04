ddccbba5241f77481e7ed683c305d78a80b9165d
mcaravario <mcaravario@dc.uba.ar>
2019-10-04 11:33:50 -0300

    testing segments branded

--- DataUrls/src/main/scala/GenerateDatasets.scala
+++ DataUrls/src/main/scala/GenerateDatasets.scala
@@ -102,30 +102,30 @@ object GenerateDatasetsUrls {
     gtDF.cache()
     gtDF = broadcast(gtDF.select("url"))
 
-    var data_keywords_content = DatasetKeywordContent.get_url_content(spark,
-                                                        country = country,
-                                                        since = since,
-                                                        ndays = ndays_dataset,
-                                                        gtDF = gtDF,
-                                                        joinType = "inner",
-                                                        name = "dataset_keyword_content_new_taxo_training")
+    // var data_keywords_content = DatasetKeywordContent.get_url_content(spark,
+    //                                                     country = country,
+    //                                                     since = since,
+    //                                                     ndays = ndays_dataset,
+    //                                                     gtDF = gtDF,
+    //                                                     joinType = "inner",
+    //                                                     name = "dataset_keyword_content_new_taxo_training")
 
-    var data_timestamp = DatasetTimestamp.get_url_timestamp(spark,
-                                                        country = country,
-                                                        since = since,
-                                                        ndays = ndays_dataset,
-                                                        gtDF = gtDF,
-                                                        joinType = "inner",
-                                                        df_urls = data_urls,
-                                                        name = "dataset_timestamp_new_taxo_training")
+    // var data_timestamp = DatasetTimestamp.get_url_timestamp(spark,
+    //                                                     country = country,
+    //                                                     since = since,
+    //                                                     ndays = ndays_dataset,
+    //                                                     gtDF = gtDF,
+    //                                                     joinType = "inner",
+    //                                                     df_urls = data_urls,
+    //                                                     name = "dataset_timestamp_new_taxo_training")
 
-    var data_user_agent = DatasetUserAgent.get_url_user_agent(spark,
-                                                      ndays_dataset,
-                                                      since,
-                                                      country,
-                                                      gtDF,
-                                                      "inner",
-                                                      name = "dataset_user_agent_new_taxo_training")
+    // var data_user_agent = DatasetUserAgent.get_url_user_agent(spark,
+    //                                                   ndays_dataset,
+    //                                                   since,
+    //                                                   country,
+    //                                                   gtDF,
+    //                                                   "inner",
+    //                                                   name = "dataset_user_agent_new_taxo_training")
 
     var data_segments_branded = DatasetSegmentsBranded.get_segment_branded(spark,
                                                       ndays_dataset,
