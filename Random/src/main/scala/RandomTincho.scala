d8807c3749417cdaf98ae5802a4d53f59069e9a1
Martin Caravario <mcaravario@dc.uba.ar>
2019-10-15 16:23:02 -0300

    fix

--- Random/src/main/scala/RandomTincho.scala
+++ Random/src/main/scala/RandomTincho.scala
@@ -205,9 +205,9 @@ object RandomTincho {
                     .withColumn("keyword",regexp_replace(col("keyword") ,"\\(", ""))
                     .withColumn("keyword",regexp_replace(col("keyword") ,"\\)", ""))
                     .withColumn("keyword",regexp_replace(col("keyword") ,",", ""))
-                    .groupBy("url","segment").agg(collect_list(col("keyword").as("keywords")))
+                    .groupBy("url","segment").agg(concat_ws(";", col("keyword")).as("keywords")))
                     .write
-                    .format("csv")
+                    .format("parquet")
                     .mode(SaveMode.Overwrite)
                     .save("/datascience/data_url_classifier/gt_new_taxo_tokenized")
  }
