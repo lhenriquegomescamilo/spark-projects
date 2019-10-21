50e3757a5b14a8ad15c6638a234a2aafd0bad9d4
mcaravario <mcaravario@dc.uba.ar>
2019-10-21 16:51:06 -0300

    Remove comments and use new files

--- DataDemo/src/main/scala/features/generateDataset.scala
+++ DataDemo/src/main/scala/features/generateDataset.scala
@@ -1,4 +1,5 @@
 package main.scala.features
+import main.scala.datasets.{UrlUtils, DatasetGA, DatasetKeywordsURL, DatasetSegmentTriplets}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SaveMode
@@ -61,137 +62,6 @@ object GenerateDataset {
 
     df
   }
-
-  // /**
-  //   * This method takes all the triplets with all the segments, for the users with ground truth in AR.
-  //   */
-  // def generateSegmentTriplets(spark: SparkSession, path: String) = {
-  //   val segments =
-  //     """26,32,36,59,61,82,85,92,104,118,129,131,141,144,145,147,149,150,152,154,155,158,160,165,166,177,178,210,213,218,224,225,226,230,245,
-  //     247,250,264,265,270,275,276,302,305,311,313,314,315,316,317,318,322,323,325,326,352,353,354,356,357,358,359,363,366,367,374,377,378,379,380,384,385,
-  //     386,389,395,396,397,398,399,401,402,403,404,405,409,410,411,412,413,418,420,421,422,429,430,432,433,434,440,441,446,447,450,451,453,454,456,457,458,
-  //     459,460,462,463,464,465,467,895,898,899,909,912,914,915,916,917,919,920,922,923,928,929,930,931,932,933,934,935,937,938,939,940,942,947,948,949,950,
-  //     951,952,953,955,956,957,1005,1116,1159,1160,1166,2064,2623,2635,2636,2660,2719,2720,2721,2722,2723,2724,2725,2726,2727,2733,2734,2735,2736,2737,2743,
-  //     3010,3011,3012,3013,3014,3015,3016,3017,3018,3019,3020,3021,3022,3023,3024,3025,3026,3027,3028,3029,3030,3031,3032,3033,3034,3035,3036,3037,3038,3039,
-  //     3040,3041,3042,3043,3044,3045,3046,3047,3048,3049,3050,3051,3055,3076,3077,3084,3085,3086,3087,3302,3303,3308,3309,3310,3388,3389,3418,3420,3421,3422,
-  //     3423,3450,3470,3472,3473,3564,3565,3566,3567,3568,3569,3570,3571,3572,3573,3574,3575,3576,3577,3578,3579,3580,3581,3582,3583,3584,3585,3586,3587,3588,
-  //     3589,3590,3591,3592,3593,3594,3595,3596,3597,3598,3599,3600,3730,3731,3732,3733,3779,3782,3843,3844,3913,3914,3915,4097,
-  //     5025,5310,5311""".replace("\n", "").split(",").toList.toSeq
-
-  //   val gt = spark.read
-  //     .format("csv")
-  //     .option("sep", "\t")
-  //     .load(path)
-  //     .withColumnRenamed("_c1", "device_id")
-  //     .withColumnRenamed("_c2", "label")
-  //     .select("device_id", "label")
-
-  //   val triplets =
-  //     spark.read
-  //       .load("/datascience/data_demo/triplets_segments/country=AR/")
-  //       .filter(col("feature").isin(segments: _*))
-
-  //   triplets
-  //     .join(gt, Seq("device_id"))
-  //     .write
-  //     .format("csv")
-  //     .mode(SaveMode.Overwrite)
-  //     .save("/datascience/data_demo/triplets_dataset_ar")
-  // }
-
-  // /**
-  //   * This method calculates all the datasets for the AR ground truth users, based only on Google Analytics (GA) data.
-  //   */
-  // def getGARelatedData(spark: SparkSession, path: String) {
-  //   // First we load the GA data
-  //   val ga = spark.read
-  //     .load(
-  //       "/datascience/data_demo/join_google_analytics/country=AR/"
-  //     )
-  //     .dropDuplicates("url", "device_id")
-
-  //   // Now we load the ground truth users
-  //   val users = spark.read
-  //     .format("csv")
-  //     .option("sep", "\t")
-  //     .load(path)
-  //     .withColumnRenamed("_c1", "device_id")
-  //     .withColumnRenamed("_c2", "label")
-  //     .select("device_id", "label")
-
-  //   // Here I calculate the data of GA just for
-  //   val joint = ga.join(users, Seq("device_id"))
-
-  //   joint.cache()
-
-  //   joint.write
-  //     .format("csv")
-  //     .mode(SaveMode.Overwrite)
-  //     .save("/datascience/data_demo/ga_dataset_AR_training")
-
-  //   // Finally we obtain the data the is related to timestamps coming from GA
-  //   val myUDF = udf(
-  //     (weekday: String, hour: String) =>
-  //       if (weekday == "Sunday" || weekday == "Saturday") "%s1".format(hour)
-  //       else "%s0".format(hour)
-  //   )
-  //   joint
-  //     .withColumn("Time", to_timestamp(from_unixtime(col("timestamp") - 3 * 3600))) // AR time transformation
-  //     .withColumn("Hour", date_format(col("Time"), "HH"))
-  //     .withColumn("Weekday", date_format(col("Time"), "EEEE"))
-  //     .withColumn("wd", myUDF(col("Weekday"), col("Hour")))
-  //     .groupBy("device_id", "wd")
-  //     .count()
-  //     .groupBy("device_id")
-  //     .pivot("wd")
-  //     .agg(sum("count"))
-  //     .write
-  //     .format("csv")
-  //     .option("header", "true")
-  //     .mode(SaveMode.Overwrite)
-  //     .save("/datascience/data_demo/ga_timestamp_AR_training")
-  // }
-
-  // def getDatasetFromURLs(spark: SparkSession, path: String) = {
-  //   // Now we load the ground truth users
-  //   val users = spark.read
-  //     .format("csv")
-  //     .option("sep", "\t")
-  //     .load(path)
-  //     .withColumnRenamed("_c1", "device_id")
-  //     .withColumnRenamed("_c2", "label")
-  //     .select("device_id", "label")
-
-  //   // Data from data audiences
-  //   val df = getDataAudiences(spark)
-  //     .filter("country = 'AR' AND event_type IN ('pv', 'batch')")
-  //     .select("device_id", "url")
-
-  //   // Here we store the data
-  //   df.join(users, Seq("device_id"))
-  //     .distinct()
-  //     .groupBy("device_id", "url")
-  //     .count()
-  //     .write
-  //     .mode(SaveMode.Overwrite)
-  //     .format("csv")
-  //     .option("sep", "\t")
-  //     .save("/datascience/custom/urls_gt_ar")
-  // }
-
-  // def getTrainingSet(spark: SparkSession, path: String) = {
-  //  // generateSegmentTriplets(spark, path)
-  //   getGARelatedData(spark, path)
-  // //  getDatasetFromURLs(spark, path)
-  // }
-
-  /**
-    *
-    *
-    *              METHODS FOR EXPANSION
-    *
-    *
-    */
 
   /**
    * This function constructs and returns a DataFrame where all the ground truth users are stored. It receives the 
@@ -431,20 +301,6 @@ object GenerateDataset {
         3589,3590,3591,3592,3593,3594,3595,3596,3597,3598,3599,3600,3730,3731,3732,3733,3779,3782,3843,3844,3913,3914,3915,4097,
         5025,5310,5311,35360,35361,35362,35363""".replace("\n", "").split(",").toList.toSeq
     
-    // Now we load the triplets, for a particular country. Here we do the group by.
-    /*
-    val triplets =
-      spark.read
-        .load(
-          "/datascience/data_demo/triplets_segments/country=%s/".format(country)
-        )
-        .filter(col("feature").isin(segments: _*))
-        .select("device_id", "feature")
-        .distinct()
-        .groupBy("device_id")
-        .agg(collect_list(col("feature")).as("feature"))
-        .withColumn("feature", concat_ws(";", col("feature")))
-    */
     
       // Now we load the triplets, for a particular country. Here we do the group by.
       val sc = spark.sparkContext
@@ -522,21 +378,38 @@ object GenerateDataset {
       .select("device_id", "url")
       .distinct()
 
-    // Here we store the data
-    gtDF.join(df, Seq("device_id"), joinType )
-                .select("device_id", "url")
-                .distinct()
-                .groupBy("device_id")
-                .agg(collect_list(col("url")).as("url"))
-                .withColumn("url", concat_ws(";", col("url")))
-                .orderBy(asc("device_id"))
-                .write
-                .mode(SaveMode.Overwrite)
-                .format("csv")
-                .option("sep", "\t")
-                .save(
-                  "/datascience/data_demo/name=%s/country=%s/urls".format(name, country)
-                )
+
+    val join = gtDF.join(df, Seq("device_id"), joinType )
+                    .select("device_id", "url")
+
+    // Process the url and get the keywords from the path.
+    val processed_join = UrlUtils.processURL(join,"url")
+                                .select("device_id", "url")
+                                .distinct()
+                                .withColumn("url", lower(col("url")))
+                                .withColumn("url_path", regexp_replace(col("url"), """^[^/]*/""", ""))
+                                .withColumn("url_keys", split(col("url_path"), "[^a-z0-9]"))
+                                .withColumn("keyword", explode(col("url_keys")))
+                                .filter(col("keyword").rlike("[a-z]{2,}"))
+                                .select("device_id","url","keyword")
+    
+    val top_keywords = processed_join.groupBy("keyword")
+                                    .agg(count(col("url")).as("count"))
+                                    .orderBy(asc("count"))
+                                    .limit(5000) // Top 5000 keywords
+
+    // Groupby device and concat the keywords                          
+    val filtered_join = processed_join.join(broadcast(top_keywords),Seq("keyword"),"inner")
+                                      .select("device_id","keyword")
+                                      .groupBy("device_id")
+                                      .agg(collect_list(col("keyword")).as("keyword"))
+                                      .withColumn("keyword", concat_ws(";", col("keyword")))
+                                      .orderBy(asc("device_id"))
+                                      .write
+                                      .format("parquet")
+                                      .save(
+                                        "/datascience/data_demo/name=%s/country=%s/keywords".format(name, country)
+                                      )
   }
 
   /** PIPELINES **/
@@ -573,14 +446,7 @@ object GenerateDataset {
     val gt = getGTDataFrame(spark,path)
     
     // Generating the GA data by joining de data from GA and the GT dataframe (inner)
-    getGARelatedData(spark, gt, country, "inner", name)
-
-    // Loading the GA dataset previously generated
-    val ga = spark.read
-                  .format("csv")
-                  .option("sep", "\t")
-                  .load("/datascience/data_demo/name=%s/country=%s/ga_dataset_probabilities".format(name, country))
-                  .withColumnRenamed("_c0","device_id")
+    val ga = DatasetGA.getGARelatedData(spark, gt, country, "inner", name)
     
     // Generating the GT dataframe (device_id, label) from the users that passed the inner join
     gt.join(ga,Seq("device_id"),"inner")
@@ -589,23 +455,15 @@ object GenerateDataset {
                 .orderBy(asc("device_id"))
                 .write
                 .mode(SaveMode.Overwrite)
-                .format("csv")
-                .option("sep", "\t")
+                .format("parquet")
                 .save(
                   "/datascience/data_demo/name=%s/country=%s/gt".format(name, country)
                 )
     // Generating the triplets dataset by joining the triplets with the GA dataset previously generated to mantain the same users
-    generateSegmentTriplets(spark, ga, country, "left", name)
-
-    // Loading the triplets dataset previously generated
-    val segments = spark.read
-                  .format("csv")
-                  .option("sep", "\t")
-                  .load("/datascience/data_demo/name=%s/country=%s/segment_triplets".format(name, country))
-                  .withColumnRenamed("_c0","device_id")
+    val segments = DatasetSegmentTriplets.generateSegmentTriplets(spark, ga, country, "left", name)
 
     // Finally we get the Url dataset (device_id, [url1;url2]) from the users that passed the join with the previous dataset
-    getDatasetFromURLs(spark, segments, country, "left", name)
+    DatasetKeywordsURL.getDatasetFromURLs(spark, segments, country, "left", name)
   }
 
   def main(args: Array[String]) {
