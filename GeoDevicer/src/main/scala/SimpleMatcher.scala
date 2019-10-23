48bf2bfcd1132ae612277386a0b75e0d0b64bf08
jif245 <31998143+jif245@users.noreply.github.com>
2019-10-23 16:44:36 -0300

    Update SimpleMatcher.scala

--- GeoDevicer/src/main/scala/SimpleMatcher.scala
+++ GeoDevicer/src/main/scala/SimpleMatcher.scala
@@ -169,7 +169,7 @@ val geosparkConf = new GeoSparkConf(spark.sparkContext.getConf)
 
 match_users_to_polygons(spark,
   "/datascience/geo/POIs/natural_geodevicer.json",
-  "10",
+  "60",
   "2",
   "argentina")
 
