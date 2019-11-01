d4d0af36aa8ef2ab63a10ffc9e0c05c5d8e6841b
jif245 <jif245@nyu.edu>
2019-11-01 11:11:19 -0300

    mexproj

--- Random/src/main/scala/elMejorRandom.scala
+++ Random/src/main/scala/elMejorRandom.scala
@@ -253,7 +253,8 @@ val HourTo = 7
 
 val raw = spark.read.format("csv").option("delimiter","\t").option("header",true).load("/datascience/geo/radios_argentina_2010_geodevicer_30d_argentina_30-8-2019-14h")
 
- val geo_hour = raw.select("device_id","device_type", "latitude", "longitude","utc_timestamp","name").withColumn("Time", to_timestamp(from_unixtime(col("utc_timestamp")))).withColumn("Hour", date_format(col("Time"), "HH")).filter(col("Hour") >= HourFrom || col("Hour") <= HourTo)
+ val geo_hour = raw.select("device_id","device_type", "latitude", "longitude","utc_timestamp","name").withColumn("Time", to_timestamp(from_unixtime(col("utc_timestamp"))))
+ .withColumn("Hour", date_format(col("Time"), "HH")).filter(col("Hour") >= HourFrom || col("Hour") <= HourTo)
                                                                  
                                                     
 val geo_counts = geo_hour.groupBy("device_id","device_type").agg(collect_list("name") as "radios_censales").withColumn("radios_censales", concat_ws(",", col("radios_censales")))
@@ -561,6 +562,7 @@ count_no_birra.write.format("csv")
 
 
 //Usuarios que fueron a un strip club. Esta es la web cookie
+ //Usuarios que fueron a un strip club. Esta es la web cookie
     val raw_data_full =  spark.read.format("csv")
   .option("header",true)
   .option("delimiter","\t")
@@ -573,13 +575,12 @@ val raw_xd = spark.read.format("csv")
   .option("delimiter",",")
   .load("/datascience/audiences/crossdeviced/mex_alcohol_60d_mexico_30-10-2019-15h_aggregated_xd").select("_c0","_c1").distinct().toDF("madid","device_id")
   
-val filter_strip_users = strip_users.join(raw_xd,Seq("device_id")).withColumn("madid",upper(col("madid")))
-  
+val filter_strip_users = strip_users.join(raw_xd,Seq("device_id","osm_id", "common_name", "type")).withColumn("madid",upper(col("madid")))
+
 val raw = spark.read.format("csv").option("header",true).option("delimiter","\t")
 .load("/datascience/geo/raw_output/mex_alcohol_60d_mexico_30-10-2019-12h")
 .withColumnRenamed("device_id","madid")
 .withColumn("madid",upper(col("madid")))
-
 
 
 
@@ -588,6 +589,8 @@ filter_strip_users.join(raw,Seq("madid")).write.format("csv")
 .option("delimiter","\t")
 .mode(SaveMode.Overwrite)
 .save("/datascience/geo/geo_processed/mex_alcohol_60d_mexico_strip_club")
+
+
 
 }
 
