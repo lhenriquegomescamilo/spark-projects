375047170724f7fc8904b82ac88bd5ec9087baaa
Leo <leopoldo.albina@retargetly.com>
2019-11-07 01:56:03 -0300

    msg

--- Reports/src/main/scala/earningsReportNew.scala
+++ Reports/src/main/scala/earningsReportNew.scala
@@ -13,7 +13,7 @@ import org.apache.log4j.{Level, Logger}
 /**
   * The idea of this script is to generate earnings Report. 
   */
-object earningsReport {
+object earningsReportNew {
 
   /**
     *
