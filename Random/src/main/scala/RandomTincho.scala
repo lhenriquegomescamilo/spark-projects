dc89b6627735e4d2350106ae4940101962e54afd
mcaravario <mcaravario@dc.uba.ar>
2019-11-11 16:33:50 -0300

    Add gt contextual

--- Random/src/main/scala/RandomTincho.scala
+++ Random/src/main/scala/RandomTincho.scala
@@ -536,6 +536,154 @@ object RandomTincho {
 
 }
 
+  def processURL(dfURL: DataFrame, field: String = "url"): DataFrame = {
+    // First of all, we get the domains, and filter out those ones that are very generic
+    val generic_domains = List(
+      "google",
+      "facebook",
+      "yahoo",
+      "android",
+      "bing",
+      "instagram",
+      "cxpublic",
+      "criteo",
+      "outbrain",
+      "flipboard",
+      "googleapis",
+      "googlequicksearchbox"
+    )
+    val query_generic_domains = generic_domains
+      .map(dom => "domain NOT LIKE '%" + dom + "%'")
+      .mkString(" AND ")
+    val filtered_domains = dfURL
+      .selectExpr("*", "parse_url(%s, 'HOST') as domain".format(field))
+      .filter(query_generic_domains)
+    // Now we filter out the domains that are IPs
+    val filtered_IPs = filtered_domains
+      .withColumn(
+        "domain",
+        regexp_replace(col("domain"), "^([0-9]+\\.){3}[0-9]+$", "IP")
+      )
+      .filter("domain != 'IP'")
+    // Now if the host belongs to Retargetly, then we will take the r_url field from the QS
+    val retargetly_domains = filtered_IPs
+      .filter("domain LIKE '%retargetly%'")
+      .selectExpr(
+        "*",
+        "parse_url(%s, 'QUERY', 'r_url') as new_url".format(field)
+      )
+      .filter("new_url IS NOT NULL")
+      .withColumn(field, col("new_url"))
+      .drop("new_url")
+    // Then we process the domains that come from ampprojects
+    val pattern =
+      """^([a-zA-Z0-9_\-]+).cdn.ampproject.org/?([a-z]/)*([a-zA-Z0-9_\-\/\.]+)?""".r
+    def ampPatternReplace(url: String): String = {
+      var result = ""
+      if (url != null) {
+        val matches = pattern.findAllIn(url).matchData.toList
+        if (matches.length > 0) {
+          val list = matches
+            .map(
+              m =>
+                if (m.groupCount > 2) m.group(3)
+                else if (m.groupCount > 0) m.group(1).replace("-", ".")
+                else "a"
+            )
+            .toList
+          result = list(0).toString
+        }
+      }
+      result
+    }
+    val ampUDF = udf(ampPatternReplace _, StringType)
+    val ampproject_domains = filtered_IPs
+      .filter("domain LIKE '%ampproject%'")
+      .withColumn(field, ampUDF(col(field)))
+      .filter("length(%s)>0".format(field))
+    // Now we union the filtered dfs with the rest of domains
+    val non_filtered_domains = filtered_IPs.filter(
+      "domain NOT LIKE '%retargetly%' AND domain NOT LIKE '%ampproject%'"
+    )
+    val filtered_retargetly = non_filtered_domains
+      .unionAll(retargetly_domains)
+      .unionAll(ampproject_domains)
+    // Finally, we remove the querystring and protocol
+    filtered_retargetly
+      .withColumn(
+        field,
+        regexp_replace(col(field), "http.*://(.\\.)*(www\\.){0,1}", "")
+      )
+      .withColumn(
+        field,
+        regexp_replace(col(field), "(\\?|#).*", "")
+      )
+      .drop("domain")
+      .withColumn(field,lower(col(field)))
+  }
+
+  def get_data_urls(
+      spark: SparkSession,
+      ndays: Int,
+      since: Int,
+      country: String
+  ): DataFrame = {
+    /// Configuraciones de spark
+    val sc = spark.sparkContext
+    val conf = sc.hadoopConfiguration
+    val fs = org.apache.hadoop.fs.FileSystem.get(conf)
+
+    /// Obtenemos la data de los ultimos ndays
+    val format = "yyyyMMdd"
+    val start = DateTime.now.minusDays(since)
+
+    val days =
+      (0 until ndays).map(start.minusDays(_)).map(_.toString(format))
+    val path = "/datascience/data_demo/data_urls/"
+    val hdfs_files = days
+      .map(day => path + "/day=%s/country=%s".format(day, country))
+      .filter(path => fs.exists(new org.apache.hadoop.fs.Path(path)))
+
+    val urls = spark.read.option("basePath", path).parquet(hdfs_files: _*)
+
+    processURL(dfURL = urls, field = "url")
+  }
+
+  def get_gt_contextual(spark:SparkSession){
+
+    val segments = List(26,   32,   36,   59,   61,   82,   85,   92,  104,  118,  129,
+                    131,  141,  144,  145,  147,  149,  150,  152,  154,  155,  158,
+                    160,  165,  166,  177,  178,  210,  213,  218,  224,  225,  226,
+                    230,  245,  247,  250,  264,  265,  270,  275,  276,  302,  305,
+                    311,  313,  314,  315,  316,  317,  318,  322,  323,  325,  326,
+                    2635, 2636, 2660, 2719, 2743, 3010, 3011, 3012, 3013, 3014, 3015,
+                    3016, 3017, 3018, 3019, 3020, 3021, 3022, 3055, 3076, 3077, 3086,
+                    3087, 3913, 4097)     
+    
+    val data_urls = get_data_urls(spark, ndays = 30, since = 1, country = "AR")
+                              .select("url", "segments")
+                              .withColumn("segments", explode(col("segments")))
+                              .filter(
+                                col("segments")
+                                  .isin(segments: _*)
+                              ).distinct()
+    val urls_contextual = spark.read.format("csv")
+                                .option("header","true")
+                                .load("/datascience/custom/scrapped_urls.csv")
+                                .select("url")
+
+    val joint = data_urls.join(urls_contextual,Seq("url"),"inner").select("url","segments")
+
+    joint.groupBy("url")
+          .agg(collect_list(col("segments")).as("segments"))
+          .withColumn("segments", concat_ws(";", col("segments")))
+          .write
+          .format("parquet")
+          .mode(SaveMode.Overwrite)
+          .save("/datascience/data_url_classifier/gt_contextual")
+
+  }
+
   def main(args: Array[String]) {
      
     // Setting logger config
@@ -547,8 +695,7 @@ object RandomTincho {
         .config("spark.sql.sources.partitionOverwriteMode","dynamic")
         .getOrCreate()
     
-    val keyword_path = "/datascience/data_url_classifier/dataset_keyword_content_contextual" 
-    keywords_embeddings(spark, keyword_path)
+    get_gt_contextual(spark)
   }
 
 }
