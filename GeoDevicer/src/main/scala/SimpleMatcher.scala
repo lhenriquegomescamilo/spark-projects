8c994f73529a0213c379077788aa1d7726aa1bc6
jif245 <31998143+jif245@users.noreply.github.com>
2019-11-13 11:50:44 -0300

    Update SimpleMatcher.scala

--- GeoDevicer/src/main/scala/SimpleMatcher.scala
+++ GeoDevicer/src/main/scala/SimpleMatcher.scala
@@ -121,7 +121,7 @@ safegraphDf.createOrReplaceTempView("data")
 */
 //if we want to use a specific dataframe with geodata, use this:
 val df_safegraph = spark.read.format("csv").option("header",false).option("delimiter","\t")
-                        .load("/datascience/geo/mexico_200d_home_29-10-2019-10h/part-00173-2dd28870-3252-45d4-95a9-0dc0c91c4454-c000.csv")
+                        .load("/datascience/geo/mexico_200d_home_29-10-2019-10h/")
                         .toDF("ad_id","id_type","freq","geocode","latitude","longitude")
                         .filter("freq>2")
 df_safegraph.createOrReplaceTempView("data")
