e081fea911e081148b939e705b4937a0b9f57f2b
jif245 <jif245@nyu.edu>
2019-11-14 12:01:36 -0300

    homes_for_chedrau

--- GeoDevicer/src/main/scala/SimpleMatcher.scala
+++ GeoDevicer/src/main/scala/SimpleMatcher.scala
@@ -137,14 +137,14 @@ safegraphDf.createOrReplaceTempView("data")
 
 val intersection = spark.sql(
       """SELECT  *   FROM poligonomagico,data   WHERE ST_Contains(poligonomagico.myshape, data.pointshape)""")
-.select("ad_id","name","id_type")
+.select("ad_id","audience","id_type")
 
 intersection.explain(extended=true)
 
 
 val output_name = (polygon_inputLocation.split("/").last).split(".json") (0).toString
 
-intersection.groupBy("ad_id","id_type","name").agg(count("name") as "frequency")
+intersection.groupBy("ad_id","id_type","audience")
 .write.format("csv")
 .option("header",true)
 .option("delimiter","\t")
