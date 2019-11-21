4d10e561057ea0530dec21dc4756dabbd2999479
jif245 <31998143+jif245@users.noreply.github.com>
2019-11-21 18:02:06 -0300

    Update SimpleMatcher.scala

--- GeoDevicer/src/main/scala/SimpleMatcher.scala
+++ GeoDevicer/src/main/scala/SimpleMatcher.scala
@@ -137,14 +137,14 @@ safegraphDf.createOrReplaceTempView("data")
 
 val intersection = spark.sql(
       """SELECT  *   FROM poligonomagico,data   WHERE ST_Contains(poligonomagico.myshape, data.pointshape)""")
-.select("ad_id","audience","id_type")
+.select("ad_id","name","id_type")
 
 intersection.explain(extended=true)
 
 
 val output_name = (polygon_inputLocation.split("/").last).split(".json") (0).toString
 
-intersection
+intersection.groupBy("ad_id","id_type","name").agg(count("name") as "frequency")
 .write.format("csv")
 .option("header",true)
 .option("delimiter","\t")
@@ -190,7 +190,7 @@ val df_safegraph = spark.read.format("parquet")
 
 df_safegraph.createOrReplaceTempView("data")
 
-var safegraphDf = spark .sql("""SELECT ad_id,ST_Point(CAST(data.longitude AS Decimal(24,20)), CAST(data.latitude AS Decimal(24,20))) as pointshape
+var safegraphDf = spark .sql("""SELECT ad_id,id_type,ST_Point(CAST(data.longitude AS Decimal(24,20)), CAST(data.latitude AS Decimal(24,20))) as pointshape
               FROM data  """)
 
 safegraphDf.createOrReplaceTempView("data")
@@ -204,7 +204,7 @@ intersection.explain(extended=true)
 
 val output_name = (polygon_inputLocation.split("/").last).split(".json") (0).toString
 
-intersection.groupBy("name", "ad_id").agg(count("name") as "frequency")
+intersection.groupBy("name", "ad_id","id_type").agg(count("name") as "frequency")
 .write.format("csv")
 .option("header",true)
 .option("delimiter","\t")
@@ -261,14 +261,10 @@ match_sample_to_polygons(spark,
       */
 
 
-
-
-      match_users_to_polygons(spark,
-        "/datascience/geo/POIs/Coordenadas_Chedraui_Poly.json",
-        "1",
-        "1",
-        "mexico")
-
+      match_sample_to_polygons(spark,
+        "/data/geo/startapp/parquet/day=*/country=CO",
+        "/datascience/geo/POIs/Centro_Comercial_Mayorca_Mega_Plaza.json",
+        "colombia")
 
   }
 }
