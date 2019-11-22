d2785a73b646870e6c36eef5aad4dccfc5a9f4f3
jif245 <31998143+jif245@users.noreply.github.com>
2019-11-22 10:36:11 -0300

    Update SimpleMatcher.scala

--- GeoDevicer/src/main/scala/SimpleMatcher.scala
+++ GeoDevicer/src/main/scala/SimpleMatcher.scala
@@ -187,7 +187,7 @@ val df_safegraph = spark.read.format("parquet")
                   .load(data_path) //"/datascience/geo/startapp/2019*"
                 //.toDF("ad_id","timestamp","country","longitude","latitude","some")
                  
-df_safegraph.show(2)
+
 df_safegraph.createOrReplaceTempView("data")
 
 var safegraphDf = spark .sql("""SELECT *,ST_Point(CAST(data.longitude AS Decimal(24,20)), CAST(data.latitude AS Decimal(24,20))) as pointshape
@@ -197,9 +197,10 @@ safegraphDf.createOrReplaceTempView("data")
 df_safegraph.show(2)
 
 val intersection = spark.sql(
-      """SELECT  *   FROM poligonomagico,data   WHERE ST_Contains(poligonomagico.myshape, data.pointshape)""").select("ad_id","name")
+      """SELECT  *   FROM poligonomagico,data   WHERE ST_Contains(poligonomagico.myshape, data.pointshape)""").select("ad_id","id_type","name")
 
 
+intersection.show(2)
 val output_name = (polygon_inputLocation.split("/").last).split(".json") (0).toString
 
 intersection.groupBy("name", "ad_id","id_type").agg(count("name") as "frequency")
