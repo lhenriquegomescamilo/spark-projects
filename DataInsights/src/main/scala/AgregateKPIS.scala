413a242d6a766b597044c373a0fdd6eabb5796a3
mcaravario <mcaravario@dc.uba.ar>
2020-01-02 17:12:06 -0300

    Add rest of datasets

--- DataInsights/src/main/scala/AgregateKPIS.scala
+++ DataInsights/src/main/scala/AgregateKPIS.scala
@@ -63,7 +63,16 @@ object AgregateKPIS {
       .withColumn("datediff",datediff(col("day"),col("time")))
       .withColumn("periodo",when(col("datediff") <= 1, "Last 1 day").otherwise(when(col("datediff") <= 7, "Last 7 days").otherwise("Last 30 days")))
       .withColumn("ID",concat(col("periodo"),lit("-"),col("campaign_id")))
-      .groupBy("campaign_id")
+      .write
+      .format("parquet")
+      .mode(SaveMode.Overwrite)
+      .save("/datascience/data_insights/data_chkpt")
+
+    val df_chkpt = spark.read.load("/datascience/data_insights/data_chkpt")
+    df_chkpt.cache()
+
+    // Data Agregada KPIS
+    df_chkpt.groupBy("campaign_id")
       .agg(count(col("id_partner")).as("hits"),
             countDistinct(col("device_id")).as("devices"),
             countDistinct(col("nid_sh2")).as("nids"),
@@ -79,6 +88,55 @@ object AgregateKPIS {
       .partitionBy("day")
       .mode("append")
       .save("/datascience/data_insights/data_kpis/")
+
+    // Data Agregada Age
+    val age_segments = List(4, 5, 6, 7, 8, 9)
+    df_chkpt.filter(col("feature").isin(age_segments: _*))
+            .groupBy(Seq("campaign_id","segments"))
+            .agg(countDistinct(col("device_id")).as("devices"),
+                  countDistinct(col("nid_sh2")).as("nids"),
+                  first("ID").as("ID"),
+                  first("campaign_name").as("campaign_name"),
+                  first("periodo").as("periodo"))
+            .withColumn("people",ceil((col("devices")/magic_ratio) + col("nids")))
+            .write
+            .format("parquet")
+            .partitionBy("day")
+            .mode("append")
+            .save("/datascience/data_insights/data_age/")
+
+    // Data Agregada Gender
+    val gender_segments = List(2, 3)
+    df_chkpt.filter(col("feature").isin(gender_segments: _*))
+            .groupBy(Seq("campaign_id","segments"))
+            .agg(countDistinct(col("device_id")).as("devices"),
+                  countDistinct(col("nid_sh2")).as("nids"),
+                  first("ID").as("ID"),
+                  first("campaign_name").as("campaign_name"),
+                  first("periodo").as("periodo"))
+            .withColumn("people",ceil((col("devices")/magic_ratio) + col("nids")))
+            .write
+            .format("parquet")
+            .partitionBy("day")
+            .mode("append")
+            .save("/datascience/data_insights/data_gender/")
+
+    // Data Agregada Device type
+    df_chkpt.groupBy(Seq("campaign_id","device_type"))
+            .agg(countDistinct(col("device_id")).as("devices"),
+                  countDistinct(col("nid_sh2")).as("nids"),
+                  first("ID").as("ID"),
+                  first("campaign_name").as("campaign_name"),
+                  first("periodo").as("periodo"))
+            .withColumn("people",ceil((col("devices")/magic_ratio) + col("nids")))
+            .write
+            .format("parquet")
+            .partitionBy("day")
+            .mode("append")
+            .save("/datascience/data_insights/data_gender/")
+
+
+
 
   }
 
