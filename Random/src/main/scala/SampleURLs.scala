5ef04fa826c2940a38b9f57c7e32acec1b1d1c9a
matlongo <matlongo@gmail.com>
2020-01-20 18:29:37 -0300

    Adding script to get URLs from MX

---
+++ Random/src/main/scala/SampleURLs.scala
@@ -0,0 +1,148 @@
+import org.apache.spark.sql._
+import org.joda.time.{Days, DateTime}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+
+object SampleURLs {
+  def processURL(dfURL: DataFrame, field: String = "url"): DataFrame = {
+    // First of all, we get the domains, and filter out those ones that are very generic
+    val generic_domains = List(
+      "google",
+      "facebook",
+      "yahoo",
+      "android",
+      "bing",
+      "instagram",
+      "cxpublic",
+      "criteo",
+      "outbrain",
+      "flipboard",
+      "googleapis",
+      "googlequicksearchbox"
+    )
+    val query_generic_domains = generic_domains
+      .map(dom => "domain NOT LIKE '%" + dom + "%'")
+      .mkString(" AND ")
+    val filtered_domains = dfURL
+      .selectExpr("*", "parse_url(%s, 'HOST') as domain".format(field))
+      .filter(query_generic_domains)
+    // Now we filter out the domains that are IPs
+    val filtered_IPs = filtered_domains
+      .withColumn(
+        "domain",
+        regexp_replace(col("domain"), "^([0-9]+\\.){3}[0-9]+$", "IP")
+      )
+      .filter("domain != 'IP'")
+    // Now if the host belongs to Retargetly, then we will take the r_url field from the QS
+    val retargetly_domains = filtered_IPs
+      .filter("domain LIKE '%retargetly%'")
+      .selectExpr(
+        "*",
+        "parse_url(%s, 'QUERY', 'r_url') as new_url".format(field)
+      )
+      .filter("new_url IS NOT NULL")
+      .withColumn(field, col("new_url"))
+      .drop("new_url")
+    // Then we process the domains that come from ampprojects
+    val pattern =
+      """^([a-zA-Z0-9_\-]+).cdn.ampproject.org/?([a-z]/)*([a-zA-Z0-9_\-\/\.]+)?""".r
+    def ampPatternReplace(url: String): String = {
+      var result = ""
+      if (url != null) {
+        val matches = pattern.findAllIn(url).matchData.toList
+        if (matches.length > 0) {
+          val list = matches
+            .map(
+              m =>
+                if (m.groupCount > 2) m.group(3)
+                else if (m.groupCount > 0) m.group(1).replace("-", ".")
+                else "a"
+            )
+            .toList
+          result = list(0).toString
+        }
+      }
+      result
+    }
+    val ampUDF = udf(ampPatternReplace _, StringType)
+    val ampproject_domains = filtered_IPs
+      .filter("domain LIKE '%ampproject%'")
+      .withColumn(field, ampUDF(col(field)))
+      .filter("length(%s)>0".format(field))
+    // Now we union the filtered dfs with the rest of domains
+    val non_filtered_domains = filtered_IPs.filter(
+      "domain NOT LIKE '%retargetly%' AND domain NOT LIKE '%ampproject%'"
+    )
+    val filtered_retargetly = non_filtered_domains
+      .unionAll(retargetly_domains)
+      .unionAll(ampproject_domains)
+    // Finally, we remove the querystring and protocol
+    filtered_retargetly
+      .withColumn(
+        field,
+        regexp_replace(col(field), "http.*://(.\\.)*(www\\.){0,1}", "")
+      )
+      .withColumn(
+        field,
+        regexp_replace(col(field), "(\\?|#).*", "")
+      )
+      .drop("domain")
+      .withColumn(field, lower(col(field)))
+  }
+
+  def get_data_urls(
+      spark: SparkSession,
+      ndays: Int,
+      since: Int,
+      country: String
+  ): DataFrame = {
+    /// Configuraciones de spark
+    val sc = spark.sparkContext
+    val conf = sc.hadoopConfiguration
+    val fs = org.apache.hadoop.fs.FileSystem.get(conf)
+
+    /// Obtenemos la data de los ultimos ndays
+    val format = "yyyyMMdd"
+    val start = DateTime.now.minusDays(since)
+
+    val days =
+      (0 until ndays).map(start.minusDays(_)).map(_.toString(format))
+    val path = "/datascience/data_demo/data_urls/"
+    val hdfs_files = days
+      .map(day => path + "/day=%s/country=%s".format(day, country))
+      .filter(path => fs.exists(new org.apache.hadoop.fs.Path(path)))
+
+    val urls = spark.read.option("basePath", path).parquet(hdfs_files: _*)
+
+    processURL(dfURL = urls, field = "url")
+    // urls
+  }
+
+  def main(args: Array[String]) {
+    val spark =
+      SparkSession.builder
+        .appName("Spark devicer")
+        .config("spark.sql.files.ignoreCorruptFiles", "true")
+        .getOrCreate()
+
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    val urls = get_data_urls(spark, 15, 5, "MX")
+      .filter("event_type IN ('pv', 'data')")
+      .select("url", "device_id", "event_type")
+      .withColumn("domain", split(col("url"), "/").getItem(0))
+    val domain_not_shareable = spark.read
+      .format("csv")
+      .load("/datascience/custom/not_shareable_domains.csv")
+      .collect()
+      .map(row => row(0).toString)
+
+    urls
+      .filter(!col("domain").isin(domain_not_shareable: _*))
+      .write
+      .format("csv")
+      .mode("overwrite")
+      .save("/datascience/custom/sample_publicis_mx")
+
+  }
+}
