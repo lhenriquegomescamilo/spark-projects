82df777330408f29ccd57f5afb64d55a9ac07a10
matlongo <matlongo@gmail.com>
2020-01-27 11:22:16 -0300

    Adding script to calculate the URLs that belong to the join test

--- KeywordIngestion/src/main/scala/TestJoin.scala
+++ KeywordIngestion/src/main/scala/TestJoin.scala
@@ -2,6 +2,7 @@ package main.scala
 
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
 import org.apache.log4j.{Level, Logger}
 
 object TestJoin {
