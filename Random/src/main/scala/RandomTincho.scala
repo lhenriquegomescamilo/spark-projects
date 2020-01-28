39d73650e311ba17b324285956725fdc46d39e64
Martin Caravario <mcaravario@dc.uba.ar>
2020-01-28 12:55:24 -0300

    Add processing for kws sharethis

--- Random/src/main/scala/RandomTincho.scala
+++ Random/src/main/scala/RandomTincho.scala
@@ -1312,7 +1312,7 @@ object RandomTincho {
   def get_kws_sharethis(spark:SparkSession){
     spark.read.json("/data/providers/sharethis/keywords/")
           .withColumn("description",lit("sharethis"))
-          .select("url","description, dt")
+          .select("url","description, dt, conecepts, entities, keywords")
           .write
           .format("csv")
           .option("delimiter", "\t")
