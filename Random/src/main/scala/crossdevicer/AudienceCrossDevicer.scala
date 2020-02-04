912a2f4578b3479b931d3c11e97a120aaa34f1ed
Leo <albinaleopoldo@gmail.com>
2020-02-04 12:35:42 -0300

    msg

--- Random/src/main/scala/crossdevicer/AudienceCrossDevicer.scala
+++ Random/src/main/scala/crossdevicer/AudienceCrossDevicer.scala
@@ -1,4 +1,4 @@
-package main.scala
+package main.scala.crossdevicer
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{upper, col}
 import org.apache.spark.sql.SaveMode
