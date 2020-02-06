298258187ad7e9df73dc6a9b0162e632882d7d18
matlongo <matlongo@gmail.com>
2020-02-06 16:26:12 -0300

    adding script to get the dataset for CL demo expansion

---
+++ Random/src/main/scala/GetDataXPCL.scala
@@ -0,0 +1,391 @@
+package main.scala
+import org.apache.spark.sql.{SparkSession, Row, SaveMode, DataFrame}
+import org.apache.spark.sql.functions._
+import org.joda.time.{Days, DateTime}
+import org.joda.time.format.DateTimeFormat
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.types._
+import org.joda.time.Days
+import org.joda.time.DateTime
+import org.apache.hadoop.conf.Configuration
+
+/**
+  * The idea of this script is to run random stuff. Most of the times, the idea is
+  * to run quick fixes, or tests.
+  */
+object GetDataXPCL {
+  def processURL(dfURL: DataFrame, field: String = "url"): DataFrame = {
+    // First of all, we get the domains, and filter out those ones that are very generic
+    val generic_domains = List(
+      "google",
+      "facebook",
+      "yahoo",
+      "android",
+      "bing",
+      "instagram",
+      "cxpublic",
+      "criteo",
+      "outbrain",
+      "flipboard",
+      "googleapis",
+      "googlequicksearchbox"
+    )
+    val query_generic_domains = generic_domains
+      .map(dom => "domain NOT LIKE '%" + dom + "%'")
+      .mkString(" AND ")
+    val filtered_domains = dfURL
+      .selectExpr("*", "parse_url(%s, 'HOST') as domain".format(field))
+      .filter(query_generic_domains)
+    // Now we filter out the domains that are IPs
+    val filtered_IPs = filtered_domains
+      .withColumn(
+        "domain",
+        regexp_replace(col("domain"), "^([0-9]+\\.){3}[0-9]+$", "IP")
+      )
+      .filter("domain != 'IP'")
+    // Now if the host belongs to Retargetly, then we will take the r_url field from the QS
+    val retargetly_domains = filtered_IPs
+      .filter("domain LIKE '%retargetly%'")
+      .selectExpr(
+        "*",
+        "parse_url(%s, 'QUERY', 'r_url') as new_url".format(field)
+      )
+      .filter("new_url IS NOT NULL")
+      .withColumn(field, col("new_url"))
+      .drop("new_url")
+    // Then we process the domains that come from ampprojects
+    val pattern =
+      """^([a-zA-Z0-9_\-]+).cdn.ampproject.org/?([a-z]/)*([a-zA-Z0-9_\-\/\.]+)?""".r
+    def ampPatternReplace(url: String): String = {
+      var result = ""
+      if (url != null) {
+        val matches = pattern.findAllIn(url).matchData.toList
+        if (matches.length > 0) {
+          val list = matches
+            .map(
+              m =>
+                if (m.groupCount > 2) m.group(3)
+                else if (m.groupCount > 0) m.group(1).replace("-", ".")
+                else "a"
+            )
+            .toList
+          result = list(0).toString
+        }
+      }
+      result
+    }
+    val ampUDF = udf(ampPatternReplace _, StringType)
+    val ampproject_domains = filtered_IPs
+      .filter("domain LIKE '%ampproject%'")
+      .withColumn(field, ampUDF(col(field)))
+      .filter("length(%s)>0".format(field))
+    // Now we union the filtered dfs with the rest of domains
+    val non_filtered_domains = filtered_IPs.filter(
+      "domain NOT LIKE '%retargetly%' AND domain NOT LIKE '%ampproject%'"
+    )
+    val filtered_retargetly = non_filtered_domains
+      .unionAll(retargetly_domains)
+      .unionAll(ampproject_domains)
+    // Finally, we remove the querystring and protocol
+    filtered_retargetly
+      .withColumn(
+        field,
+        regexp_replace(col(field), "http.*://(.\\.)*(www\\.){0,1}", "")
+      )
+      .withColumn(
+        field,
+        regexp_replace(col(field), "(\\?|#).*", "")
+      )
+      .drop("domain")
+      .withColumn(field, lower(col(field)))
+  }
+
+  def get_data_urls(
+      spark: SparkSession,
+      ndays: Int,
+      since: Int,
+      country: String
+  ): DataFrame = {
+    /// Configuraciones de spark
+    val sc = spark.sparkContext
+    val conf = sc.hadoopConfiguration
+    val fs = org.apache.hadoop.fs.FileSystem.get(conf)
+
+    /// Obtenemos la data de los ultimos ndays
+    val format = "yyyyMMdd"
+    val start = DateTime.now.minusDays(since)
+
+    val days =
+      (0 until ndays).map(start.minusDays(_)).map(_.toString(format))
+    val path = "/datascience/data_demo/data_urls/"
+    val hdfs_files = days
+      .map(day => path + "/day=%s/country=%s".format(day, country))
+      .filter(path => fs.exists(new org.apache.hadoop.fs.Path(path)))
+
+    val urls = spark.read.option("basePath", path).parquet(hdfs_files: _*)
+
+    processURL(dfURL = urls, field = "url")
+    // urls
+  }
+
+  def get_data_user_agents(
+      spark: SparkSession,
+      ndays: Int,
+      since: Int,
+      country: String
+  ): DataFrame = {
+    /// Configuraciones de spark
+    val sc = spark.sparkContext
+    val conf = sc.hadoopConfiguration
+    val fs = org.apache.hadoop.fs.FileSystem.get(conf)
+
+    /// Obtenemos la data de los ultimos ndays
+    val format = "yyyyMMdd"
+    val start = DateTime.now.minusDays(since)
+
+    val days =
+      (0 until ndays).map(start.minusDays(_)).map(_.toString(format))
+    val path = "/datascience/data_useragents/"
+    val hdfs_files = days
+      .map(day => path + "/day=%s/country=%s".format(day, country))
+      .filter(path => fs.exists(new org.apache.hadoop.fs.Path(path)))
+
+    val user_agents =
+      spark.read.option("basePath", path).parquet(hdfs_files: _*)
+
+    user_agents
+  }
+
+  def getDataTriplets(
+      spark: SparkSession,
+      country: String,
+      nDays: Int = -1,
+      path: String = "/datascience/data_triplets/segments/"
+  ) = {
+    // First we obtain the configuration to be allowed to watch if a file exists or not
+    val conf = spark.sparkContext.hadoopConfiguration
+    val fs = FileSystem.get(conf)
+
+    val df = if (nDays > 0) {
+      // read files from dates
+      val format = "yyyyMMdd"
+      val endDate = DateTime.now.minusDays(1)
+      val days =
+        (0 until nDays.toInt).map(endDate.minusDays(_)).map(_.toString(format))
+      // Now we obtain the list of hdfs folders to be read
+      val hdfs_files = days
+        .map(day => path + "/day=%s/country=%s".format(day, country))
+        .filter(path => fs.exists(new org.apache.hadoop.fs.Path(path)))
+
+      val dfs = hdfs_files.map(
+        f =>
+          spark.read
+            .parquet(f)
+            .select("device_id", "feature")
+            .withColumn("count", lit(1))
+            .withColumnRenamed("feature", "segment")
+      )
+      dfs.reduce((df1, df2) => df1.unionAll(df2))
+    } else {
+      // read all date files
+      spark.read.load(path + "/day=*/country=%s/".format(country))
+    }
+    df
+  }
+
+  /**
+    * GET UA DATA FOR A GIVEN AUDIENCE
+    */
+  def getUAData(spark: SparkSession, expansion: Boolean) = {
+    val user_agents =
+      get_data_user_agents(spark, 60, 1, "CL")
+
+    val data = user_agents
+      .select("device_id", "user_agent")
+      .dropDuplicates("device_id")
+
+    if (expansion) {
+      data.write
+        .format("parquet")
+        .mode("overwrite")
+        .save("/datascience/custom/cl_users_ua_xp")
+    } else {
+      val audience = spark.read
+        .format("csv")
+        .option("sep", "\t")
+        .load("/datascience/devicer/processed/equifax_cl_gender")
+        .withColumnRenamed("_c1", "device_id")
+        .withColumnRenamed("_c2", "ids")
+
+      data
+        .join(audience, Seq("device_id"), "inner")
+        .write
+        .format("parquet")
+        .mode("overwrite")
+        .save("/datascience/custom/cl_users_ua_train")
+    }
+  }
+
+  /**
+    * GET URL DATA FOR A GIVEN AUDIENCE
+    */
+  def getURLData(spark: SparkSession, expansion: Boolean) = {
+    val urls =
+      get_data_urls(spark, 60, 1, "CL").select(
+        "device_id",
+        "url"
+      )
+
+    if (expansion) {
+      val selected_urls = spark.read
+        .format("csv")
+        .load("/datascience/custom/urls_used.csv")
+        .withColumnRenamed("_c0", "url")
+
+      urls
+        .join(broadcast(selected_urls), Seq("url"), "inner")
+        .select("device_id", "url")
+        .distinct()
+        .groupBy("device_id")
+        .agg(collect_list("url") as "urls")
+        .write
+        .format("parquet")
+        .mode("overwrite")
+        .save("/datascience/custom/cl_users_xp")
+    } else {
+      val audience = spark.read
+        .format("csv")
+        .option("sep", "\t")
+        .load("/datascience/devicer/processed/equifax_cl_gender")
+        .withColumnRenamed("_c1", "device_id")
+        .withColumnRenamed("_c2", "ids")
+
+      urls
+        .join(audience, Seq("device_id"), "inner")
+        .select("device_id", "url", "ids")
+        .dropDuplicates("device_id", "url")
+        .write
+        .format("parquet")
+        .mode("overwrite")
+        .save("/datascience/custom/cl_users_urls_train")
+    }
+
+  }
+
+  def get_joint(spark: SparkSession) = {
+    val ua_df =
+      spark.read.format("parquet").load("/datascience/custom/cl_users_ua_xp")
+    val urls =
+      spark.read.format("parquet").load("/datascience/custom/cl_users_urls_xp")
+
+    val joint = ua_df
+      .join(urls, Seq("device_id"), "left")
+      .withColumn("urls", concat_ws("|", col("urls")))
+
+    val gt = spark.read
+      .format("csv")
+      .load("/datascience/custom/devices_gt_br.csv")
+      .withColumnRenamed("_c0", "device_id")
+      .repartition(20)
+
+    joint
+    // .join(gt, Seq("device_id"), "left_anti")
+    .write
+      .format("csv")
+      .option("sep", "\t")
+      .mode("overwrite")
+      .save("/datascience/custom/dataset_expansion_ar")
+  }
+
+  def getDataSegments(spark: SparkSession, expansion: Boolean) = {
+    val data_audiences = spark.read
+      .format("csv")
+      .option("sep", "\t")
+      .option("header", "false")
+      .load("/datascience/custom/dataset_expansion_ar")
+      .withColumnRenamed("_c0", "device_id")
+      .withColumnRenamed("_c1", "ua")
+      .withColumnRenamed("_c2", "urls")
+
+    val audience = spark.read
+      .format("csv")
+      .option("sep", "\t")
+      .load("/datascience/devicer/processed/equifax_cl_gender")
+      .withColumnRenamed("_c1", "device_id")
+      .withColumnRenamed("_c2", "ids")
+
+    val taxonomy = Seq(2, 3, 4, 5, 6, 7, 8, 9, 26, 32, 36, 59, 61, 82, 85, 92,
+      104, 118, 129, 131, 141, 144, 145, 147, 149, 150, 152, 154, 155, 158, 160,
+      165, 166, 177, 178, 210, 213, 218, 224, 225, 226, 230, 245, 247, 250, 264,
+      265, 270, 275, 276, 302, 305, 311, 313, 314, 315, 316, 317, 318, 322, 323,
+      325, 326, 352, 353, 354, 356, 357, 358, 359, 363, 366, 367, 374, 377, 378,
+      379, 380, 384, 385, 386, 389, 395, 396, 397, 398, 399, 401, 402, 403, 404,
+      405, 409, 410, 411, 412, 413, 418, 420, 421, 422, 429, 430, 432, 433, 434,
+      440, 441, 446, 447, 450, 451, 453, 454, 456, 457, 458, 459, 460, 462, 463,
+      464, 465, 467, 895, 898, 899, 909, 912, 914, 915, 916, 917, 919, 920, 922,
+      923, 928, 929, 930, 931, 932, 933, 934, 935, 937, 938, 939, 940, 942, 947,
+      948, 949, 950, 951, 952, 953, 955, 956, 957, 1005, 1116, 1159, 1160, 1166,
+      2623, 2635, 2636, 2660, 2719, 2720, 2721, 2722, 2723, 2724, 2725, 2726,
+      2727, 2733, 2734, 2735, 2736, 2737, 2743, 3010, 3011, 3012, 3013, 3014,
+      3015, 3016, 3017, 3018, 3019, 3020, 3021, 3022, 3023, 3024, 3025, 3026,
+      3027, 3028, 3029, 3030, 3031, 3032, 3033, 3034, 3035, 3036, 3037, 3038,
+      3039, 3040, 3041, 3055, 3076, 3077, 3084, 3085, 3086, 3087, 3302, 3303,
+      3308, 3309, 3310, 3388, 3389, 3418, 3420, 3421, 3422, 3423, 3470, 3472,
+      3473, 3564, 3565, 3566, 3567, 3568, 3569, 3570, 3571, 3572, 3573, 3574,
+      3575, 3576, 3577, 3578, 3579, 3580, 3581, 3582, 3583, 3584, 3585, 3586,
+      3587, 3588, 3589, 3590, 3591, 3592, 3593, 3594, 3595, 3596, 3597, 3598,
+      3599, 3600, 3779, 3782, 3913, 3914, 3915, 4097, 104014, 104015, 104016,
+      104017, 104018, 104019)
+
+    val data_segments = getDataTriplets(spark, country = "CL", nDays = 60)
+      .filter(col("segment").isin(taxonomy: _*))
+      .select("device_id", "segment")
+      .groupBy("device_id")
+      .agg(collect_list("segment") as "segment")
+      .withColumn("segment", concat_ws("|", col("segment")))
+
+    if (expansion) {
+      data_audiences
+        .join(data_segments, Seq("device_id"), "left")
+        .write
+        .format("csv")
+        .option("sep", "\t")
+        .mode("overwrite")
+        .save("/datascience/custom/cl_users_all_xp")
+    } else {
+      audience
+        .join(data_segments, Seq("device_id"), "inner")
+        .write
+        .format("csv")
+        .option("sep", "\t")
+        .mode("overwrite")
+        .save("/datascience/custom/cl_users_segments_train")
+    }
+
+  }
+
+  /*****************************************************/
+  /******************     MAIN     *********************/
+  /*****************************************************/
+  def main(args: Array[String]) {
+    val spark =
+      SparkSession.builder
+        .appName("Spark devicer")
+        .config("spark.sql.files.ignoreCorruptFiles", "true")
+        .getOrCreate()
+
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    getUAData(
+      spark = spark,
+      expansion = false
+    )
+    getURLData(
+      spark = spark,
+      expansion = false
+    )
+    // get_joint(spark)
+    getDataSegments(spark, expansion = false)
+  }
+}
