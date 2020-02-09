737512f77728838ad5595d0238e350685930b849
matlongo <matlongo@gmail.com>
2020-02-09 16:07:56 -0300

    modifying reporter to not fail when there is no data

--- Reporter/src/main/scala/Reporter.scala
+++ Reporter/src/main/scala/Reporter.scala
@@ -44,7 +44,7 @@ object Reporter {
 
     // Empty file that will be used when there is no Data
     val file =
-      "/datascience/reporter/empty_files/pipeline_reporter_overlap.snappy.parquet"
+      "/datascience/reporter/empty_file_reporter/day=20200201/id_partner=1/"
 
     // Get list of valid days
     var hdfs_files_reporter = file :: (0 to nDays)
