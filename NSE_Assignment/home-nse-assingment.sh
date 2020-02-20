ed44435ed466980bc134199452e53a0655ac680d
jif245 <jif245@nyu.edu>
2020-02-20 16:42:45 -0300

    new sh

---
+++ NSE_Assignment/home-nse-assingment.sh
@@ -0,0 +1,16 @@
+
+spark-submit\
+  --class 'main.scala.NSEFromHomes'\
+  --master yarn\
+  --deploy-mode cluster\
+  --driver-memory 8g \
+  --executor-memory 8g \
+  --num-executors 10 \
+  --executor-cores 3 \
+  --queue default \
+  --jars geospark-1.2.0.jar,geospark-sql_2.3-1.2.0.jar\
+  --conf spark.serializer=org.apache.spark.serializer.KryoSerializer\
+  --conf spark.kryo.registrator=org.datasyslab.geosparkviz.core.Serde.GeoSparkVizKryoRegistrator \
+  --conf spark.yarn.maxAppAttempts=1 \
+  --conf spark.sql.broadcastTimeout=3000000  \
+  '/home/rely/spark-projects/NSE_Assignment/target/scala-2.11/geodevicer_2.11-1.0.jar'
