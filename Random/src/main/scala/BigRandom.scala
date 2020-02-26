199890aab0e5cb3d650a90bdda37567112f4f3ed
Leo <albinaleopoldo@gmail.com>
2020-02-26 11:01:43 -0300

    msg

--- Random/src/main/scala/BigRandom.scala
+++ Random/src/main/scala/BigRandom.scala
@@ -303,6 +303,84 @@ def getSelectedKeywords(
     df_grouped
   }
 
+
+///////////////////////7////////////////////////////////////
+
+import scala.collection.mutable.ListBuffer
+
+val domains = List("ac", "ad", "ae", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "as", "asia", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "biz", "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cat", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "com", "coop", "cr", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "edu", "ee", "eg", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gob", "gov", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "info", "int", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jobs", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mil", "mk", "ml", "mm", "mn", "mo", "mobi", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "net", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "org", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "pro", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "su", "sv", "sy", "sz", "tc", "td", "tel", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "uk", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "xxx", "ye", "yt", "za", "zm", "zw")
+
+def parseURL(url: String): String = {
+
+  // First we obtain the query string and the URL divided in 
+  val split = url.split("\\?")
+
+  val fields = split(0).split('/')
+
+  // Now we can get the URL path and the section with no path at all
+  val path = (if (url.startsWith("http")) fields.slice(3, fields.length)
+  else fields.slice(1, fields.length))
+  val non_path = (if (url.startsWith("http")) fields(2) else fields(0)).split("\\:")(0)
+
+  // From the non-path, we can get the extension, the domain, and the subdomain
+  val parts = non_path.split("\\.").toList
+  var extension: ListBuffer[String] = new ListBuffer[String]()
+  var count = parts.length
+  if (count > 0) {
+    var part = parts(count - 1)
+    // First we get the extension
+    while (domains.contains(part) && count > 1) {
+      extension += part
+      count = count - 1
+      part = parts(count - 1)
+    }
+
+  // Now we obtain the domain and subdomain.
+  val domain = if (count > 0) parts(count - 1) else ""
+
+  domain
+
+  } else ("")
+  }
+
+val udfGetDomain = udf(
+    (url: String) =>
+    parseURL(url)
+  )
+
+ def getDataURLS(
+      spark: SparkSession,
+      country: String,
+      nDays: Integer,
+      since: Integer
+  ): DataFrame = {
+
+    val conf = spark.sparkContext.hadoopConfiguration
+    val fs = FileSystem.get(conf)
+
+    // Get the days to be loaded
+    val format = "yyyyMMdd"
+    val end = DateTime.now.minusDays(since)
+    val days = (0 until nDays).map(end.minusDays(_)).map(_.toString(format))
+    val path = "/datascience/url_ingester/data"
+
+    // Now we obtain the list of hdfs folders to be read
+    val hdfs_files = days
+      .map(day => path + "/day=%s/country=%s".format(day, country)) //for each day from the list it returns the day path.
+      .filter(file_path => fs.exists(new org.apache.hadoop.fs.Path(file_path))) //analogue to "os.exists"
+
+    val df = spark.read
+      .option("basePath", path)
+      .parquet(hdfs_files: _*)
+      .withColumn("domain",udfGetDomain(col("url")))
+      .na
+      .drop()
+
+    df
+  }
+
+
+
  /*****************************************************/
   /******************     MAIN     *********************/
   /*****************************************************/
@@ -311,6 +389,21 @@ def getSelectedKeywords(
     .appName("BigRandom")
     .config("spark.sql.files.ignoreCorruptFiles", "true")
     .getOrCreate()
+
+
+  val df_old = getDataURLS(spark, "AR", 15 , 36 )
+  .filter("domain=='zonajobs'")
+
+  println(df_old.groupBy("domain").agg(sum(col("count")) as "total_hits").show())
+
+  val df_new = getDataURLS(spark, "AR", 15 , 16 )
+  .filter("domain=='zonajobs'")
+  
+  println(df_new.groupBy("domain").agg(sum(col("count")) as "total_hits").show())
+
+
+
+    /**
 
     def getString =
         udf((array: Seq[String]) => array.map(_.toString).mkString(","))
@@ -343,6 +436,8 @@ def getSelectedKeywords(
           .option("header",true)
           .mode(SaveMode.Overwrite)
           .save("/datascience/misc/df_new_all")
+
+    */
 
     /**
 
