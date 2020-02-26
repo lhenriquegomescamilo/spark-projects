1c630192916d0a8ccb7ea448d4737be49ddb61de
jif245 <jif245@nyu.edu>
2020-02-26 16:55:27 -0300

    pipeline

--- NSE_Assignment/monthly-home-to-sftp.sh
+++ NSE_Assignment/monthly-home-to-sftp.sh
@@ -9,12 +9,15 @@ mkdir $TEMP_PATH
 /home/rely/hadoop/bin/hdfs dfs -copyToLocal /datascience/geo/NSEHomes/monthly/equifax/to_push/$fecha/{AR,CL,CO,PE,MX} $TEMP_PATH
 
 
-for ar in $(ls -1 $TEMP_PATH'country=AR/'); do cat $TEMP_PATH'country=AR/'*.csv > $TEMP_PATH"EQUIFAX_HOMES_AR_$fecha.csv"
-#for cl in $(ls -1 $TEMP_PATH'country=CL/'); do mv $TEMP_PATH'country=CL/'$cl $TEMP_PATH"pii_$fecha"'_CL.csv'; done
-#for pe in $(ls -1 $TEMP_PATH'country=PE/'); do mv $TEMP_PATH'country=PE/'$pe $TEMP_PATH"pii_$fecha"'_PE.csv'; done
+cat $TEMP_PATH'AR/'*.csv > $TEMP_PATH"EQUIFAX_HOMES_AR_$fecha.csv"
+cat $TEMP_PATH'CL/'*.csv > $TEMP_PATH"EQUIFAX_HOMES_CL_$fecha.csv"
+cat $TEMP_PATH'CO/'*.csv > $TEMP_PATH"EQUIFAX_HOMES_CO_$fecha.csv"
+cat $TEMP_PATH'PE/'*.csv > $TEMP_PATH"EQUIFAX_HOMES_PE_$fecha.csv"
+cat $TEMP_PATH'MX/'*.csv > $TEMP_PATH"EQUIFAX_HOMES_MX_$fecha.csv"
 
+#for pe in $(ls -1 $TEMP_PATH'country=PE/'); do mv $TEMP_PATH'country=PE/'$pe $TEMP_PATH"pii_$fecha"'_PE.csv'; done
 
 
 #echo put $TEMP_PATH/equifax_nse_$fecha.csv | sftp -i /home/rely/.ssh/equifax.key equifax@input-01:/equifax_homes
 #rm -r $TEMP_PATH
-#rm -r $TEMP_PATH/equifax_nse_$fecha.csv
+#rm -r $TEMP_PATH/
