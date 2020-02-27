4c9b1d550322476be81aa21b7f53fb15348cf80e
jif245 <jif245@nyu.edu>
2020-02-27 12:06:55 -0300

    messages

--- NSE_Assignment/monthly-home-nse-assingment.sh
+++ NSE_Assignment/monthly-home-nse-assingment.sh
@@ -16,5 +16,7 @@ for json in $(hdfs dfs -ls -C /datascience/geo/NSEHomes/monthly/to_process/); do
   --conf spark.yarn.maxAppAttempts=1 \
   --conf spark.sql.broadcastTimeout=3000000  \
   "/home/rely/spark-projects/NSE_Assignment/target/scala-2.11/geodevicer_2.11-1.0.jar" --path_geo_json $json;
+
+echo $ "has been read"
 done
 
