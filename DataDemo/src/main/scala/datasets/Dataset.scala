65fe8222ad34dec6296a589f8fca59b3cb1e4bfc
matlongo <matlongo@gmail.com>
2020-02-28 15:32:29 -0300

    Adding the script to obtain the dataset for demo expansions

---
+++ DataDemo/src/main/scala/datasets/Dataset.scala
@@ -0,0 +1,284 @@
+package main.scala
+import org.apache.spark.sql.{SparkSession, Row, SaveMode, DataFrame}
+import org.apache.spark.sql.functions._
+import org.joda.time.{Days, DateTime}
+import org.joda.time.format.DateTimeFormat
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.types._
+import org.joda.time.Days
+import org.joda.time.DateTime
+import org.apache.hadoop.conf.Configuration
+
+/**
+  * The idea of this script is to generate the datasets that will be used to
+  * train and predict the demographic attributes. It uses data from URLs and User Agents.
+  */
+object Dataset {
+
+  /** This function preprocesses the URLs that belong to a DataFrame. It takes
+    * as input a DataFrame and the name of the column that contains the URL. Based on that
+    * it filters those URLs that are noise, that come from generic domains, that are IPs,
+    * and extract the domain from it.
+    *
+    * As a result, it returns a new DataFrame with the URLs filtered and parsed.
+    *
+    * - dfURL: DataFrame containing at least one column holding the URLs.
+    * - field: column name where the URL is stored.
+    */
+  def processURL(dfURL: DataFrame, field: String = "url"): DataFrame = {
+    // First of all, we get the domains, and filter out those ones that are very generic
+    val generic_domains = List(
+      "google",
+      "facebook",
+      "yahoo",
+      "android",
+      "bing",
+      "instagram",
+      "cxpublic",
+      "criteo",
+      "outbrain",
+      "flipboard",
+      "googleapis",
+      "googlequicksearchbox"
+    )
+    val query_generic_domains = generic_domains
+      .map(dom => "domain NOT LIKE '%" + dom + "%'")
+      .mkString(" AND ")
+    val filtered_domains = dfURL
+      .selectExpr("*", "parse_url(%s, 'HOST') as domain".format(field))
+      .filter(query_generic_domains)
+    // Now we filter out the domains that are IPs
+    val filtered_IPs = filtered_domains
+      .withColumn(
+        "domain",
+        regexp_replace(col("domain"), "^([0-9]+\\.){3}[0-9]+$", "IP")
+      )
+      .filter("domain != 'IP'")
+    // Now if the host belongs to Retargetly, then we will take the r_url field from the QS
+    val retargetly_domains = filtered_IPs
+      .filter("domain LIKE '%retargetly%'")
+      .selectExpr(
+        "*",
+        "parse_url(%s, 'QUERY', 'r_url') as new_url".format(field)
+      )
+      .filter("new_url IS NOT NULL")
+      .withColumn(field, col("new_url"))
+      .drop("new_url")
+    // Then we process the domains that come from ampprojects
+    val pattern =
+      """^([a-zA-Z0-9_\-]+).cdn.ampproject.org/?([a-z]/)*([a-zA-Z0-9_\-\/\.]+)?""".r
+    def ampPatternReplace(url: String): String = {
+      var result = ""
+      if (url != null) {
+        val matches = pattern.findAllIn(url).matchData.toList
+        if (matches.length > 0) {
+          val list = matches
+            .map(
+              m =>
+                if (m.groupCount > 2) m.group(3)
+                else if (m.groupCount > 0) m.group(1).replace("-", ".")
+                else "a"
+            )
+            .toList
+          result = list(0).toString
+        }
+      }
+      result
+    }
+    val ampUDF = udf(ampPatternReplace _, StringType)
+    val ampproject_domains = filtered_IPs
+      .filter("domain LIKE '%ampproject%'")
+      .withColumn(field, ampUDF(col(field)))
+      .filter("length(%s)>0".format(field))
+    // Now we union the filtered dfs with the rest of domains
+    val non_filtered_domains = filtered_IPs.filter(
+      "domain NOT LIKE '%retargetly%' AND domain NOT LIKE '%ampproject%'"
+    )
+    val filtered_retargetly = non_filtered_domains
+      .unionAll(retargetly_domains)
+      .unionAll(ampproject_domains)
+    // Finally, we remove the querystring and protocol
+    filtered_retargetly
+      .withColumn(
+        field,
+        regexp_replace(col(field), "http.*://(.\\.)*(www\\.){0,1}", "")
+      )
+      .withColumn(
+        field,
+        regexp_replace(col(field), "(\\?|#).*", "")
+      )
+      .drop("domain")
+      .withColumn(field, lower(col(field)))
+  }
+
+  /** This function returns the URL daily dataset. This dataset contains all of the
+    * organic URLs for a given country and the specified date range. The returned URLs
+    * are previously parsed and filtered so that no noise is passed.
+    *
+    * - spark: SparkSession that will be used to load the dataset.
+    * - ndays: number of days to be loaded.
+    * - since: number of days to be skiped from today's date.
+    * - country: URL source country.
+    *
+    * Returns a DataFrame with two columns: device_id and URL.
+    */
+  def get_data_urls(
+      spark: SparkSession,
+      ndays: Int,
+      since: Int,
+      country: String
+  ): DataFrame = {
+    // Spark settings
+    val sc = spark.sparkContext
+    val conf = sc.hadoopConfiguration
+    val fs = org.apache.hadoop.fs.FileSystem.get(conf)
+
+    // List of days to be loaded
+    val format = "yyyyMMdd"
+    val start = DateTime.now.minusDays(since)
+
+    // Loading the dataset
+    val days =
+      (0 until ndays).map(start.minusDays(_)).map(_.toString(format))
+    val path = "/datascience/data_demo/data_urls/"
+    val hdfs_files = days
+      .map(day => path + "/day=%s/country=%s".format(day, country))
+      .filter(path => fs.exists(new org.apache.hadoop.fs.Path(path)))
+
+    val urls = spark.read.option("basePath", path).parquet(hdfs_files: _*)
+
+    processURL(dfURL = urls, field = "url").select("device_id", "url")
+  }
+
+  /** This function returns the User Agent daily dataset. This dataset contains
+    * all of the user-agents for a given country and the specified date range.
+    *
+    * - spark: SparkSession that will be used to load the dataset.
+    * - ndays: number of days to be loaded.
+    * - since: number of days to be skiped from today's date.
+    * - country: URL source country.
+    *
+    * Returns a DataFrame with two columns: device_id and user_agent.
+    */
+  def get_data_user_agents(
+      spark: SparkSession,
+      ndays: Int,
+      since: Int,
+      country: String
+  ): DataFrame = {
+    // Spark settings
+    val sc = spark.sparkContext
+    val conf = sc.hadoopConfiguration
+    val fs = org.apache.hadoop.fs.FileSystem.get(conf)
+
+    // List of days to be loaded
+    val format = "yyyyMMdd"
+    val start = DateTime.now.minusDays(since)
+
+    // Loading the dataset
+    val days =
+      (0 until ndays).map(start.minusDays(_)).map(_.toString(format))
+    val path = "/datascience/data_useragents/"
+    val hdfs_files = days
+      .map(day => path + "/day=%s/country=%s".format(day, country))
+      .filter(path => fs.exists(new org.apache.hadoop.fs.Path(path)))
+
+    val user_agents =
+      spark.read.option("basePath", path).parquet(hdfs_files: _*)
+
+    user_agents.select("device_id", "user_agent")
+  }
+
+  /**
+    * This function obtains the dataset that will be used for
+    * the given country and the date range. It obtains the user_agent
+    * and the list of URLs for each user.
+    *
+    * - spark: SparkSession that will be used to load the dataset.
+    * - ndays: number of days to be loaded.
+    * - since: number of days to be skiped from today's date.
+    * - country: URL source country.
+    */
+  def getDataset(
+      spark: SparkSession,
+      country: String,
+      nDays: Integer = 30,
+      since: Integer = 1
+  ) = {
+    // First we get the user-agents, one per user.
+    val user_agents =
+      get_data_user_agents(spark, nDays, since, country)
+        .select("device_id", "user_agent")
+        .dropDuplicates("device_id")
+
+    // Now we get the list of URLs for each user
+    val urls =
+      get_data_urls(spark, nDays, since, country)
+        .select(
+          "device_id",
+          "url"
+        )
+        .select("device_id", "url")
+        .distinct()
+        .groupBy("device_id")
+        .agg(collect_list("url") as "urls")
+
+    val joint = user_agents
+      .join(urls, Seq("device_id"), "left")
+      .select("device_id", "user_agent", "urls")
+      .withColumn("country", lit(country))
+      .withColumn(
+        "day",
+        lit(DateTime.now.minusDays(since).toString("yyyyMMdd"))
+      )
+
+    joint.write
+      .format("parquet")
+      .partitionBy("country", "day")
+      .mode("append")
+      .save("/datascience/data_demo/datasets/")
+  }
+
+  type OptionMap = Map[Symbol, Any]
+
+  /**
+    * This method parses the parameters sent.
+    */
+  def nextOption(map: OptionMap, list: List[String]): OptionMap = {
+    def isSwitch(s: String) = (s(0) == '-')
+    list match {
+      case Nil => map
+      case "--from" :: value :: tail =>
+        nextOption(map ++ Map('from -> value.toString), tail)
+      case "--nDays" :: value :: tail =>
+        nextOption(map ++ Map('nDays -> value.toString), tail)
+      case "--country" :: value :: tail =>
+        nextOption(map ++ Map('audiences -> value), tail)
+    }
+  }
+
+  /*****************************************************/
+  /******************     MAIN     *********************/
+  /*****************************************************/
+  def main(args: Array[String]) {
+    // Parse the parameters
+    val options = nextOption(Map(), args.toList)
+    val from = if (options.contains('from)) options('from).toString.toInt else 1
+    val nDays =
+      if (options.contains('nDays)) options('nDays).toString.toInt else 30
+    val country: String =
+      if (options.contains('country)) options('country).toString else "AR"
+
+    val spark =
+      SparkSession.builder
+        .appName("Spark devicer")
+        .config("spark.sql.files.ignoreCorruptFiles", "true")
+        .getOrCreate()
+
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    getDataset(spark, country, nDays, from)
+
+  }
+}
--- DataDemo/src/main/scala/monthly/GenerateGTJson.scala
+++ DataDemo/src/main/scala/monthly/GenerateGTJson.scala
@@ -1,32 +1,50 @@
 package main.scala.monthly
-import org.apache.hadoop.fs.{ FileSystem, Path}
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hadoop.conf.Configuration
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.joda.time.DateTime
 import org.apache.spark.sql.{SaveMode, DataFrame}
 
-object GenerateGTJson{
+object GenerateGTJson {
 
-  def generate_json_genre_ar(spark:SparkSession, conf:Configuration, current_month: String){
+  def generate_json_genre_ar(
+      spark: SparkSession,
+      conf: Configuration,
+      current_month: String
+  ) {
 
     var fs = FileSystem.get(conf)
-    var os = fs.create(new Path("/datascience/devicer/to_process/AR_genero_%s.json".format(current_month)))
+    var os = fs.create(
+      new Path(
+        "/datascience/devicer/to_process/AR_genero_%s.json"
+          .format(current_month)
+      )
+    )
 
-    val content = """{"xd": 0, "partnerId": "", "query": "(country = 'AR' and event_type IN ('retroactive') and array_contains(segments, 20125))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 2, "since": 1, "priority": 14, "common": "country = 'AR'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    val content =
+      """{"xd": 0, "partnerId": "", "query": "(country = 'AR' and event_type IN ('retroactive') and array_contains(segments, 20125))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 2, "since": 1, "priority": 14, "common": "country = 'AR'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
     {"xd": 0, "partnerId": "", "query": "(country = 'AR' and event_type IN ('retroactive') and array_contains(segments, 20126))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 3, "since": 1, "priority": 14, "common": "country = 'AR'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}"""
     os.write(content.getBytes)
     fs.close()
 
   }
 
-
-  def generate_json_age_ar(spark:SparkSession, conf:Configuration, current_month: String){
+  def generate_json_age_ar(
+      spark: SparkSession,
+      conf: Configuration,
+      current_month: String
+  ) {
 
     var fs = FileSystem.get(conf)
-    var os = fs.create(new Path("/datascience/devicer/to_process/AR_edad_%s.json".format(current_month)))
+    var os = fs.create(
+      new Path(
+        "/datascience/devicer/to_process/AR_edad_%s.json".format(current_month)
+      )
+    )
 
-    val content = """{"xd": 0, "partnerId": "", "query": "(country = 'AR' and event_type IN ('xd', 'retroactive') and array_contains(segments, 20115))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 8, "since": 1, "priority": 14, "common": "country = 'AR'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    val content =
+      """{"xd": 0, "partnerId": "", "query": "(country = 'AR' and event_type IN ('xd', 'retroactive') and array_contains(segments, 20115))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 8, "since": 1, "priority": 14, "common": "country = 'AR'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
     {"xd": 0, "partnerId": "", "query": "(country = 'AR' and event_type IN ('retroactive') and array_contains(segments, 20116))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 9, "since": 1, "priority": 14, "common": "country = 'AR'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
     {"xd": 0, "partnerId": "", "query": "(country = 'AR' and event_type IN ('retroactive') and array_contains(segments, 20114))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 7, "since": 1, "priority": 14, "common": "country = 'AR'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
     {"xd": 0, "partnerId": "", "query": "(country = 'AR' and event_type IN ('retroactive') and array_contains(segments, 20112))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 5, "since": 1, "priority": 14, "common": "country = 'AR'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
@@ -37,26 +55,43 @@ object GenerateGTJson{
 
   }
 
-
-  def generate_json_genre_mx(spark:SparkSession, conf:Configuration, current_month: String){
+  def generate_json_genre_mx(
+      spark: SparkSession,
+      conf: Configuration,
+      current_month: String
+  ) {
 
     val fs = FileSystem.get(conf)
-    val os = fs.create(new Path("/datascience/devicer/to_process/MX_genero_%s.json".format(current_month)))
+    val os = fs.create(
+      new Path(
+        "/datascience/devicer/to_process/MX_genero_%s.json"
+          .format(current_month)
+      )
+    )
 
-    val content = """{"xd": 0, "partnerId": "", "query": "(country = 'MX' and array_contains(all_segments, 69228))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 2, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    val content =
+      """{"xd": 0, "partnerId": "", "query": "(country = 'MX' and array_contains(all_segments, 69228))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 2, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
     {"xd": 0, "partnerId": "", "query": "(country = 'MX' and array_contains(all_segments, 69207))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 3, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}"""
     os.write(content.getBytes)
     fs.close()
 
   }
 
-
-  def generate_json_age_mx(spark:SparkSession, conf:Configuration, current_month: String){
+  def generate_json_age_mx(
+      spark: SparkSession,
+      conf: Configuration,
+      current_month: String
+  ) {
 
     val fs = FileSystem.get(conf)
-    val os = fs.create(new Path("/datascience/devicer/to_process/MX_edad_%s.json".format(current_month)))
+    val os = fs.create(
+      new Path(
+        "/datascience/devicer/to_process/MX_edad_%s.json".format(current_month)
+      )
+    )
 
-    val content = """{"xd": 0, "partnerId": "", "query": "(country = 'MX' and array_contains(all_segments, 106399))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 8, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    val content =
+      """{"xd": 0, "partnerId": "", "query": "(country = 'MX' and array_contains(all_segments, 106399))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 8, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
     {"xd": 0, "partnerId": "", "query": "(country = 'MX' and array_contains(all_segments, 106399))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 9, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
     {"xd": 0, "partnerId": "", "query": "(country = 'MX' and array_contains(all_segments, 106399))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 7, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
     {"xd": 0, "partnerId": "", "query": "(country = 'MX' and array_contains(all_segments, 106395))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 5, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
@@ -67,13 +102,98 @@ object GenerateGTJson{
 
   }
 
+  def generate_json_gender_cl(
+      spark: SparkSession,
+      conf: Configuration,
+      current_month: String
+  ) = {
+    val fs = FileSystem.get(conf)
+    val os = fs.create(
+      new Path(
+        "/datascience/devicer/to_process/CL_genero_%s.json"
+          .format(current_month)
+      )
+    )
+
+    val content =
+      """{"xd": 0, "partnerId": "", "query": "(country = 'CL' and array_contains(all_segments, 136487))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 2, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    {"xd": 0, "partnerId": "", "query": "(country = 'CL' and array_contains(all_segments, 136489))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 3, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}"""
+    os.write(content.getBytes)
+    fs.close()
+  }
+
+  def generate_json_age_cl(
+      spark: SparkSession,
+      conf: Configuration,
+      current_month: String
+  ) = {
+    val fs = FileSystem.get(conf)
+    val os = fs.create(
+      new Path(
+        "/datascience/devicer/to_process/CL_age_%s.json"
+          .format(current_month)
+      )
+    )
+
+    val content =
+      """{"xd": 0, "partnerId": "", "query": "(country = 'CL' and array_contains(all_segments, 136491))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 4, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    {"xd": 0, "partnerId": "", "query": "(country = 'CL' and array_contains(all_segments, 136493))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 5, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    {"xd": 0, "partnerId": "", "query": "(country = 'CL' and array_contains(all_segments, 136495))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 6, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    {"xd": 0, "partnerId": "", "query": "(country = 'CL' and array_contains(all_segments, 136497))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 7, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    {"xd": 0, "partnerId": "", "query": "(country = 'CL' and array_contains(all_segments, 136499))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 8, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}"""
+    os.write(content.getBytes)
+    fs.close()
+  }
+
+  def generate_json_age_br(
+      spark: SparkSession,
+      conf: Configuration,
+      current_month: String
+  ) = {
+    val fs = FileSystem.get(conf)
+    val os = fs.create(
+      new Path(
+        "/datascience/devicer/to_process/BR_age_%s.json"
+          .format(current_month)
+      )
+    )
+
+    val content =
+      """{"xd": 0, "partnerId": "", "query": "(country = 'BR' and array_contains(all_segments, 24609))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 4, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    {"xd": 0, "partnerId": "", "query": "(country = 'BR' and (array_contains(all_segments, 24610) or array_contains(all_segments, 24611)))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 5, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    {"xd": 0, "partnerId": "", "query": "(country = 'BR' and (array_contains(all_segments, 24612) or array_contains(all_segments, 24613)))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 6, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    {"xd": 0, "partnerId": "", "query": "(country = 'BR' and (array_contains(all_segments, 24614) or array_contains(all_segments, 24615)))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 7, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    {"xd": 0, "partnerId": "", "query": "(country = 'BR' and (array_contains(all_segments, 24616) or array_contains(all_segments, 24617)))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 8, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    {"xd": 0, "partnerId": "", "query": "(country = 'BR' and (array_contains(all_segments, 24618) or array_contains(all_segments, 24619)))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 9, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}"""
+    os.write(content.getBytes)
+    fs.close()
+  }
+
+  def generate_json_gender_br(
+      spark: SparkSession,
+      conf: Configuration,
+      current_month: String
+  ) = {
+    val fs = FileSystem.get(conf)
+    val os = fs.create(
+      new Path(
+        "/datascience/devicer/to_process/BR_gender_%s.json"
+          .format(current_month)
+      )
+    )
+
+    val content =
+      """{"xd": 0, "partnerId": "", "query": "country = 'BR' and (array_contains(all_segments, 24621) or (array_contains(all_segments, 2) AND id_partner = 916))", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 2, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}
+    {"xd": 0, "partnerId": "", "query": "country = 'BR' and (array_contains(all_segments, 24622) or (array_contains(all_segments, 3) AND id_partner = 916))"", "ndays": 30, "queue": "datascience", "pipeline": 0, "segmentId": 3, "since": 1, "priority": 14, "common": "country = 'MX'", "as_view": 119, "push": 0, "xdFilter": "index_type = 'coo'"}"""
+    os.write(content.getBytes)
+    fs.close()
+  }
 
   def main(args: Array[String]) {
     val spark = SparkSession.builder
       .appName("Monthly Generation of GT Jsons")
       .config("spark.sql.files.ignoreCorruptFiles", "true")
       .getOrCreate()
-
 
     // Defining parameter configuration
     val conf = new Configuration()
@@ -82,14 +202,20 @@ object GenerateGTJson{
     // Get actual month to put in gt jsons name
     val current_month = DateTime.now().toString("yyyyMM")
 
+    // AR
     generate_json_age_ar(spark, conf, current_month)
-
     generate_json_genre_ar(spark, conf, current_month)
 
+    // MX
     generate_json_genre_mx(spark, conf, current_month)
 
-    // Uncomment when we get data GT from MX
-    //generate_json_age_mx(spark, conf, current_month)
+    // CL
+    generate_json_gender_cl(spark, conf, current_month)
+    generate_json_age_cl(spark, conf, current_month)
+
+    // BR
+    generate_json_gender_br(spark, conf, current_month)
+    generate_json_age_br(spark, conf, current_month)
 
   }
 }
