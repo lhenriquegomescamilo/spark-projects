5d969065f9d8e92892e923ee1fd4652b6f2a89ca
Leo <albinaleopoldo@gmail.com>
2020-03-02 12:18:09 -0300

    msg

--- NLP/src/main/scala/Ranlp.scala
+++ NLP/src/main/scala/Ranlp.scala
@@ -129,13 +129,15 @@ object Ranlp {
 
     val doc = pipeline.fit(df).transform(df)
 
+    println(doc.take(4).show())
 
+    /**
     doc.write.format("csv")
       .option("header", "true")
       .option("sep", "\t")
       .mode(SaveMode.Overwrite)
       .save("/datascience/misc/testnlp.csv")
-
+    */
 
 
 
