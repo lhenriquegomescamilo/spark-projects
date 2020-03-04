5b7cb26fdae96e7844ccc262204458622a188c8c
Leo <leonardo@retargetly.com>
2020-03-04 14:17:05 -0300

    LAL warning log messages

--- LookAlike/src/main/scala/Item2Item.scala
+++ LookAlike/src/main/scala/Item2Item.scala
@@ -332,8 +332,10 @@ object Item2Item {
     if (!df.columns.contains("count"))
       df = df.withColumn("count", lit(1))
     if (!df.columns.contains("activable"))
+      println("Lookalike LOG: WARNING - Data triplets without activable column - (country = %s - from = %s - nDays = %s) - setting default value  = 1".format(country, from, nDays))
       df = df.withColumn("activable", lit(1))
     if (!df.columns.contains("device_type"))
+      println("Lookalike LOG: WARNING - Data triplets without device_type column - (country = %s - from = %s - nDays = %s) - setting default value = 'web'".format(country, from, nDays))
       df = df.withColumn("device_type", lit("web"))
 
     df.select("device_id", "device_type", "activable", "feature", "count")
