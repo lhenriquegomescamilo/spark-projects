90df8402db6ba4479a6a7356cd90825be04ba7ce
Leo <leonardo@retargetly.com>
2020-03-04 11:58:46 -0300

    URLContentEmbedding

--- URLContentEmbedding/src/main/scala/MeanWordsEmbedder.scala
+++ URLContentEmbedding/src/main/scala/MeanWordsEmbedder.scala
@@ -13,7 +13,7 @@ import org.apache.commons.lang3.StringUtils
 import scala.collection.mutable.WrappedArray
 
 /**
-* MeanWordsEmbedders
+* MeanWordsEmbedder
 *
 * This class generates a vectorial representation of the scraped content of urls.
 * The data is read from html parsed by Newspaper3k python library. It extracts all words from headers and title of html.
@@ -26,22 +26,22 @@ import scala.collection.mutable.WrappedArray
 object MeanWordsEmbedder {
 
   // Path of parsed HTMLs to process.
-  private var INPUT_PATH = "/datascience/scraper/parsed/processed/"
+  private val INPUT_PATH = "/datascience/scraper/parsed/processed/"
 
   // Quality Filter: Minimum number of detected words in HTML by the pretrained embedding model.
-  private var MIN_UNIQUE_EMB_WORDS = 3
+  private val MIN_UNIQUE_EMB_WORDS = 3
 
   // Spanish pretrained embedding model (without stopwords, accents removed, lowered)
-  private var SP_EMBEDDING_PATH = "/datascience/scraper/embeddings/models/sp_nostopwords_embeddings_small.csv"
+  private val SP_EMBEDDING_PATH = "/datascience/scraper/embeddings/models/sp_mean_model_embeddings.csv"
 
   // List of spanish special characters used in language detection
-  private var SP_CHARACERS = List("á", "é", "í", "ó", "ú","ü", "ñ", "¿", "¡")
+  private val SP_CHARACTERS = List("á", "é", "í", "ó", "ú","ü", "ñ", "¿", "¡")
 
   // List of portuguese special characters used in language detection
-  private var PT_CHARACERS = List("á", "â", "ã", "à", "ç", "é", "ê", "í", "ó", "ô", "õ", "ú", "¿", "¡")
+  private val PT_CHARACTERS = List("á", "â", "ã", "à", "ç", "é", "ê", "í", "ó", "ô", "õ", "ú", "¿", "¡")
 
   // List of spanish stopwords used in language detection (lowered, accents removed)
-  private var SP_STOPWORDS = List("a", "al", "algo", "algunas", "algunos", "ante", "antes", "como", "con", "contra", "cual", "cuando",
+  private val SP_STOPWORDS = List("a", "al", "algo", "algunas", "algunos", "ante", "antes", "como", "con", "contra", "cual", "cuando",
                                   "de", "del", "desde", "donde", "durante", "e", "el", "ella", "ellas", "ellos", "en", "entre", "era",
                                   "erais", "eramos", "eran", "eras", "eres", "es", "esa", "esas", "ese", "eso", "esos", "esta", "estaba",
                                   "estabais", "estabamos", "estaban", "estabas", "estad", "estada", "estadas", "estado", "estados", "estais",
@@ -71,7 +71,7 @@ object MeanWordsEmbedder {
                                   "y", "ya", "yo")
 
   // List of portuguese stopwords used in language detection (lowered, accents removed)
-  private var PT_STOPWORDS = List("a", "ao", "aos", "aquela", "aquelas", "aquele", "aqueles", "aquilo", "as", "ate", "com", "como", "da", "das", "de", "dela", "delas", "dele",
+  private val PT_STOPWORDS = List("a", "ao", "aos", "aquela", "aquelas", "aquele", "aqueles", "aquilo", "as", "ate", "com", "como", "da", "das", "de", "dela", "delas", "dele",
                                   "deles", "depois", "do", "dos", "e", "ela",  "elas", "ele", "eles", "em", "entre", "era", "eram", "eramos", "essa", "essas", "esse", "esses",
                                   "esta", "estamos", "estao", "estas", "estava", "estavam", "estavamos", "este", "esteja", "estejam", "estejamos", "estes", "esteve", "estive",
                                   "estivemos", "estiver", "estivera", "estiveram", "estiveramos", "estiverem", "estivermos", "estivesse", "estivessem", "estivessemos", "estou",
@@ -86,7 +86,7 @@ object MeanWordsEmbedder {
                                   "tivesse", "tivessem", "tivessemos", "tu", "tua", "tuas", "um", "uma", "voce", "voces", "vos")
 
   // List of english stopwords used in language detection (lowered)
-  private var EN_STOPWORDS = List("a", "about", "above", "after", "again", "against", "ain", "all", "am", "an", "and", "any", "are", "aren", "aren't",
+  private val EN_STOPWORDS = List("a", "about", "above", "after", "again", "against", "ain", "all", "am", "an", "and", "any", "are", "aren", "aren't",
                                   "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can", "couldn",
                                   "couldn't", "d", "did", "didn", "didn't", "do", "does", "doesn", "doesn't", "doing", "don", "don't", "down", "during",
                                   "each", "few", "for", "from", "further", "had", "hadn", "hadn't", "has", "hasn", "hasn't", "have", "haven", "haven't",
@@ -197,78 +197,19 @@ object MeanWordsEmbedder {
   def detectLanguage(spark: SparkSession,
                     dfTokenized: DataFrame) =  { 
     
-   // List of spanish special characters used in language detection
-   var SP_CHARACERS = List("á", "é", "í", "ó", "ú","ü", "ñ", "¿", "¡")
+   // it creates local variables (it's needed for udf functions)
+   var sp_characters = SP_CHARACTERS
+   var pt_characters = PT_CHARACERS
+   var sp_stpowords = SP_STOPWORDS
+   var pt_stopwords = PT_STOPWORDS
+   var en_stopwords = EN_STOPWORDS                               )
 
-   // List of portuguese special characters used in language detection
-   var PT_CHARACERS = List("á", "â", "ã", "à", "ç", "é", "ê", "í", "ó", "ô", "õ", "ú", "¿", "¡")
+   val nuniqueCharSP = udf((text: String) => sp_characters.map(ch => if(text contains ch) 1 else 0 ).sum )
+   val nuniqueCharPT = udf((text: String) => pt_characters.map(ch => if(text contains ch) 1 else 0 ).sum )
 
-   // List of spanish stopwords used in language detection (lowered, accents removed)
-   var SP_STOPWORDS = List("a", "al", "algo", "algunas", "algunos", "ante", "antes", "como", "con", "contra", "cual", "cuando",
-                                  "de", "del", "desde", "donde", "durante", "e", "el", "ella", "ellas", "ellos", "en", "entre", "era",
-                                  "erais", "eramos", "eran", "eras", "eres", "es", "esa", "esas", "ese", "eso", "esos", "esta", "estaba",
-                                  "estabais", "estabamos", "estaban", "estabas", "estad", "estada", "estadas", "estado", "estados", "estais",
-                                  "estamos", "estan", "estando", "estar", "estara", "estaran", "estaras", "estare", "estareis", "estaremos",
-                                  "estaria", "estariais", "estariamos", "estarian", "estarias", "estas", "este", "esteis", "estemos", "esten",
-                                  "estes", "esto", "estos", "estoy", "estuve", "estuviera", "estuvierais", "estuvieramos", "estuvieran",
-                                  "estuvieras", "estuvieron", "estuviese", "estuvieseis", "estuviesemos", "estuviesen", "estuvieses",
-                                  "estuvimos","estuviste", "estuvisteis", "estuvo", "fue", "fuera", "fuerais", "fueramos", "fueran",
-                                  "fueras", "fueron", "fuese", "fueseis", "fuesemos", "fuesen", "fueses", "fui", "fuimos", "fuiste",
-                                  "fuisteis", "ha", "habeis", "habia", "habiais", "habiamos", "habian", "habias", "habida", "habidas",
-                                  "habido", "habidos", "habiendo", "habra", "habran", "habras", "habre", "habreis", "habremos", "habria",
-                                  "habriais", "habriamos", "habrian", "habrias", "han", "has", "hasta", "hay", "haya", "hayais", "hayamos",
-                                  "hayan", "hayas", "he", "hemos", "hube", "hubiera", "hubierais", "hubieramos", "hubieran", "hubieras", "hubieron",
-                                  "hubiese", "hubieseis", "hubiesemos", "hubiesen", "hubieses", "hubimos", "hubiste", "hubisteis", "hubo",
-                                  "la", "las", "le", "les", "lo", "los", "mas", "me", "mi", "mia", "mias", "mio", "mios", "mis", "mucho",
-                                  "muchos", "muy", "nada", "ni", "no", "nos", "nosotras", "nosotros", "nuestra", "nuestras", "nuestro", "nuestros",
-                                  "o", "os", "otra", "otras", "otro", "otros", "para", "pero", "poco", "por", "porque", "que", "quien", "quienes",
-                                  "se", "sea", "seais", "seamos", "sean", "seas", "sentid", "sentida", "sentidas", "sentido", "sentidos", "sera",
-                                  "seran", "seras", "sere", "sereis", "seremos", "seria", "seriais", "seriamos", "serian", "serias", "si", "siente",
-                                  "sin", "sintiendo", "sobre", "sois", "somos", "son", "soy", "su", "sus", "suya", "suyas", "suyo", "suyos", "tambien",
-                                  "tanto", "te", "tendra", "tendran", "tendras", "tendre", "tendreis", "tendremos", "tendria", "tendriais", "tendriamos",
-                                  "tendrian", "tendrias", "tened", "teneis", "tenemos", "tenga", "tengais", "tengamos", "tengan", "tengas", "tengo", "tenia",
-                                  "teniais", "teniamos", "tenian", "tenias", "tenida", "tenidas", "tenido", "tenidos", "teniendo", "ti", "tiene", "tienen",
-                                  "tienes", "todo", "todos", "tu", "tus", "tuve", "tuviera", "tuvierais", "tuvieramos", "tuvieran", "tuvieras", "tuvieron",
-                                  "tuviese", "tuvieseis", "tuviesemos", "tuviesen", "tuvieses", "tuvimos", "tuviste", "tuvisteis", "tuvo", "tuya", "tuyas",
-                                  "tuyo", "tuyos", "un", "una", "uno", "unos", "vos", "vosotras", "vosotros", "vuestra", "vuestras", "vuestro", "vuestros",
-                                  "y", "ya", "yo")
-
-   // List of portuguese stopwords used in language detection (lowered, accents removed)
-   var PT_STOPWORDS = List("a", "ao", "aos", "aquela", "aquelas", "aquele", "aqueles", "aquilo", "as", "ate", "com", "como", "da", "das", "de", "dela", "delas", "dele",
-                                  "deles", "depois", "do", "dos", "e", "ela",  "elas", "ele", "eles", "em", "entre", "era", "eram", "eramos", "essa", "essas", "esse", "esses",
-                                  "esta", "estamos", "estao", "estas", "estava", "estavam", "estavamos", "este", "esteja", "estejam", "estejamos", "estes", "esteve", "estive",
-                                  "estivemos", "estiver", "estivera", "estiveram", "estiveramos", "estiverem", "estivermos", "estivesse", "estivessem", "estivessemos", "estou",
-                                  "eu", "foi", "fomos", "for", "fora", "foram", "foramos", "forem", "formos", "fosse", "fossem", "fossemos", "fui", "ha", "haja", "hajam", "hajamos",
-                                  "hao", "havemos", "hei", "houve", "houvemos", "houver", "houvera", "houveram", "houveramos", "houverao", "houverei", "houverem", "houveremos",
-                                  "houveria", "houveriam", "houveriamos", "houvermos", "houvesse", "houvessem", "houvessemos", "isso", "isto", "ja", "lhe", "lhes", "mais", "mas",
-                                  "me", "mesmo", "meu", "meus", "minha", "minhas", "muito", "na", "nao", "nas", "nem", "no", "nos", "nossa", "nossas", "nosso", "nossos", "num",
-                                  "numa", "o", "os", "ou", "para", "pela", "pelas", "pelo", "pelos", "por", "qual", "quando", "que", "quem", "sao", "se", "seja", "sejam",
-                                  "sejamos", "sem", "sera", "serao", "serei", "seremos", "seria", "seriam", "seriamos", "seu", "seus", "so", "somos", "sou", "sua", "suas",
-                                  "tambem", "te", "tem", "temos", "tenha", "tenham", "tenhamos", "tenho", "tera", "terao", "terei", "teremos", "teria", "teriam", "teriamos",
-                                  "teu", "teus", "teve", "tinha", "tinham", "tinhamos", "tive", "tivemos", "tiver", "tivera", "tiveram", "tiveramos", "tiverem", "tivermos",
-                                  "tivesse", "tivessem", "tivessemos", "tu", "tua", "tuas", "um", "uma", "voce", "voces", "vos")
-
-   // List of english stopwords used in language detection (lowered)
-   var EN_STOPWORDS = List("a", "about", "above", "after", "again", "against", "ain", "all", "am", "an", "and", "any", "are", "aren", "aren't",
-                                  "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can", "couldn",
-                                  "couldn't", "d", "did", "didn", "didn't", "do", "does", "doesn", "doesn't", "doing", "don", "don't", "down", "during",
-                                  "each", "few", "for", "from", "further", "had", "hadn", "hadn't", "has", "hasn", "hasn't", "have", "haven", "haven't",
-                                  "having", "he", "her", "here", "hers", "herself", "him", "himself", "his", "how", "i", "if", "in", "into", "is", "isn",
-                                  "isn't", "it", "it's", "its", "itself", "just", "ll", "m", "ma", "me", "mightn", "mightn't", "more", "most", "mustn", "mustn't",
-                                  "my", "myself", "needn", "needn't", "no", "nor", "not", "now", "o", "of", "off", "on", "once", "only", "or", "other", "our",
-                                  "ours", "ourselves", "out", "over", "own", "re", "s", "same", "shan", "shan't", "she", "she's", "should", "should've", "shouldn",
-                                  "shouldn't", "so", "some", "such", "t", "than", "that", "that'll", "the", "their", "theirs", "them", "themselves", "then", "there",
-                                  "these", "they", "this", "those", "through", "to", "too", "under", "until", "up", "ve", "very", "was", "wasn", "wasn't", "we",
-                                  "were", "weren", "weren't", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "won", "won't",
-                                  "wouldn", "wouldn't", "y", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"
-                                  )
-
-   val nuniqueCharSP = udf((text: String) => SP_CHARACERS.map(ch => if(text contains ch) 1 else 0 ).sum )
-   val nuniqueCharPT = udf((text: String) => PT_CHARACERS.map(ch => if(text contains ch) 1 else 0 ).sum )
-
-    val countStopwordsSP = udf((words: WrappedArray[String]) => words.map(w => if(SP_STOPWORDS contains w ) 1 else 0 ).sum)
-    val countStopwordsPT = udf((words: WrappedArray[String]) => words.map(w => if(PT_STOPWORDS contains w ) 1 else 0 ).sum)
-    val countStopwordsEN = udf((words: WrappedArray[String]) => words.map(w => if(EN_STOPWORDS contains w ) 1 else 0 ).sum)
+    val countStopwordsSP = udf((words: WrappedArray[String]) => words.map(w => if(sp_stpowords contains w ) 1 else 0 ).sum)
+    val countStopwordsPT = udf((words: WrappedArray[String]) => words.map(w => if(pt_stopwords contains w ) 1 else 0 ).sum)
+    val countStopwordsEN = udf((words: WrappedArray[String]) => words.map(w => if(en_stopwords contains w ) 1 else 0 ).sum)
 
     val predLanguage = udf((swSP: Int, swPT: Int, swEN: Int, chSP: Int, chPT: Int) => 
       // no stopwords or accents
@@ -398,7 +339,7 @@ object MeanWordsEmbedder {
     val from = if (options.contains('from)) options('from) else 1
 
     val spark = SparkSession.builder
-        .appName("URL Content Embedding")
+        .appName("URL Mean Word Embeddings")
         .config("spark.sql.files.ignoreCorruptFiles", "true")
         .config("spark.sql.sources.partitionOverwriteMode","dynamic")
         .getOrCreate()
