03404aa2a5df709907b1e0276548b981ab74541f
Leo <albinaleopoldo@gmail.com>
2020-03-18 13:02:13 -0300

    msg

---
+++ Taxonomy/run-spark.sh
@@ -0,0 +1,14 @@
+spark-submit\
+  --master yarn\
+  --deploy-mode cluster\
+  --driver-memory 8g\
+  --executor-memory 8g\
+  --num-executors 12\
+  --executor-cores 3\
+  --queue spark\
+  --conf spark.yarn.maxAppAttempts=1\
+  /home/rely/spark-projects/Taxonomy/target/scala-2.11/keywiser_2.11-1.0.jar 
+
+
+
+
