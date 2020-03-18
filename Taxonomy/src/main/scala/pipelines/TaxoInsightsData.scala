b7a12d19d0942c9d77986c412e841183c686c4a7
Leo <albinaleopoldo@gmail.com>
2020-03-18 15:02:49 -0300

    msg

--- Taxonomy/src/main/scala/pipelines/TaxoInsightsData.scala
+++ Taxonomy/src/main/scala/pipelines/TaxoInsightsData.scala
@@ -4,6 +4,7 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
 import org.joda.time.{Days, DateTime}
+import org.joda.time.format.DateTimeFormat
 
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hadoop.conf.Configuration
@@ -15,7 +16,7 @@ import org.apache.log4j.{Level, Logger}
   * The idea of this script is to run random stuff. Most of the times, the idea is
   * to run quick fixes, or tests.
   */
-object TaxoInsights {
+object TaxoInsightsData {
 
 
 /**
@@ -69,8 +70,46 @@ def processDay(
       since: Integer
   ): DataFrame = {
     
+    val map_events = Map(
+          "batch" -> 0,
+          "data" -> 1,
+          "tk" -> 2,
+          "pv" -> 3,
+          "retroactive" -> 4)
+    val mapUDF_events = udf((event_type: String) => map_events(event_type))
 
-    ///
+    // 1) Read Data URLS and transform
+    val path_urls = "/datascience/data_demo/data_urls"
+    val data_urls = getData(spark,nDays,since,path_urls)
+    .select("device_id","url","id_partner","event_type","segments","country")
+    .withColumn("segment", explode(col("segments")))
+    .selectExpr("*", "parse_url(url, 'HOST') as domain")
+    .withColumn("event_type",mapUDF_events(col("event_type"))) //este mapeo lo hago pero ya deberia estar hecho en data urls.
+    .select("device_id","domain","id_partner","event_type","segment","country")
+
+    // 2) Read Data Triplets and transform
+    val path_triplets = "/datascience/data_triplets/segments"
+    val data_triplets = getData(spark,nDays,since,path_triplets)
+    .filter("activable== 1")
+    .select("device_id","feature","id_partner","event_type","country")
+    .withColumnRenamed("feature", "segment")
+    .withColumn("domain",lit("not_url"))
+
+    // 3) Concat dfs and aggregate devices
+    val df = data_urls.union(data_triplets)
+    .groupBy("domain","event_type","segment","id_partner","event_type","country")
+    .agg(approx_count_distinct(col("device_id"), 0.02).as("devices_count"))
+
+    // 4) Order and Save
+    df.orderBy("segment") //preguntar si esta bien
+      .write
+      .format("parquet")
+      .mode("append")
+      .withColumn("day", lit(DateTime.now.minusDays(since)))
+      .partitionBy("day", "country")
+      .save("/datascience/taxo_insights/data")
+
+  )
 
 }  
 
@@ -105,12 +144,12 @@ def processDay(
 
     // First we obtain the Spark session
     val spark = SparkSession.builder
-      .appName("TaxoInsights")
+      .appName("TaxoInsightsData")
       .config("spark.sql.files.ignoreCorruptFiles", "true")
       .config("spark.sql.sources.partitionOverwriteMode", "dynamic")
       .getOrCreate()
 
-
+    processDay(spark,nDays,since)
 
   }
 }
