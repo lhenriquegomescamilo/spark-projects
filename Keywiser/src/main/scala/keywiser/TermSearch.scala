d38aee2906d6acedda64044481ae0a78280335f0
Leo <albinaleopoldo@gmail.com>
2020-03-24 19:30:44 -0300

    msg

--- Keywiser/src/main/scala/keywiser/TermSearch.scala
+++ Keywiser/src/main/scala/keywiser/TermSearch.scala
@@ -239,6 +239,47 @@ object TermSearch {
 
     **/
 
+    val domains = List("ac", "ad", "ae", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "as", "asia", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "biz", "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cat", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "com", "coop", "cr", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "edu", "ee", "eg", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gob", "gov", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "info", "int", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jobs", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mil", "mk", "ml", "mm", "mn", "mo", "mobi", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "net", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "org", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "pro", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "su", "sv", "sy", "sz", "tc", "td", "tel", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "uk", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "xxx", "ye", "yt", "za", "zm", "zw")
+
+    def parseURL(url: String): String = {
+
+      // First we obtain the query string and the URL divided in 
+      val split = url.split("\\?")
+
+      val fields = split(0).split('/')
+
+      // Now we can get the URL path and the section with no path at all
+      val path = (if (url.startsWith("http")) fields.slice(3, fields.length)
+      else fields.slice(1, fields.length))
+      val non_path = (if (url.startsWith("http")) fields(2) else fields(0)).split("\\:")(0)
+
+      // From the non-path, we can get the extension, the domain, and the subdomain
+      val parts = non_path.split("\\.").toList
+      var extension: ListBuffer[String] = new ListBuffer[String]()
+      var count = parts.length
+      if (count > 0) {
+        var part = parts(count - 1)
+        // First we get the extension
+        while (domains.contains(part) && count > 1) {
+          extension += part
+          count = count - 1
+          part = parts(count - 1)
+        }
+
+      // Now we obtain the domain and subdomain.
+      val domain = if (count > 0) parts(count - 1) else ""
+ 
+      domain
+
+      } else ("")
+      }
+
+
+    val udfGetDomain = udf(
+        (url: String) =>
+        parseURL(url)
+      )
+
     val taxo = spark.read.format("csv")
     .option("header",true)
     .load("/datascience/geo/Reports/Equifax/DataMixta/RelyTaxonomy_06_02_2020.csv")
@@ -249,8 +290,8 @@ object TermSearch {
     val df =spark.read.format("parquet")
     .option("header",true)
     .load(path)
-    .selectExpr("*", "parse_url(url, 'HOST') as domain")
-    .withColumn("segments", split(col("segments"), " "))
+    .withColumn("domain", udfGetDomain(col("url")))
+    .withColumn("segments", split(col("segments"), ","))
     .withColumn("segmentId", explode(col("segments")))
     .join(taxo,Seq("segmentId"))
     .groupBy("domain","device_id","day").agg(collect_list("name").as("behaviour"))
