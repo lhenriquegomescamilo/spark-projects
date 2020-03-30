c1cc2da1b7d796db4bf965e340d6c188b7e3ff93
jif245 <jif245@nyu.edu>
2020-03-30 11:58:19 -0300

    homesjobsgeohash

--- Random/src/main/scala/elMejorRandom.scala
+++ Random/src/main/scala/elMejorRandom.scala
@@ -1335,15 +1335,17 @@ val one_geohash_user = get_safegraph_data(spark,nDays,"1",country)
 val entidad = spark.read.format("csv").option("header",true).option("delimiter",",")
 .load("/datascience/geo/geo_processed/AR_departamentos_barrios_mexico_sjoin_polygon")
 
+val output_file = "/datascience/geo/geo_processed/geohashes_user_location_%sD_%s_%s".format(nDays,country,today)
+
 one_geohash_user
 .join(entidad,Seq("geo_hash_7"))
 .write
 .mode(SaveMode.Overwrite)
 .format("csv")
 .option("header",true)
-.save(output_file_partido)
+.save(output_file)
 
-val output_file = "/datascience/geo/geo_processed/geohashes_user_location_%sD_%s_%s".format(nDays,country,today)
+
 }
 
 
