d008170da65250fd6ecbe52e0c956d082350e43c
Martin Caravario <mcaravario@dc.uba.ar>
2020-03-30 12:22:13 -0300

    update imports

--- Tapad/src/main/scala/TapadWhitelist.scala
+++ Tapad/src/main/scala/TapadWhitelist.scala
@@ -6,8 +6,6 @@ import org.apache.spark.sql.SaveMode
 import org.apache.spark.ml.Pipeline
 import org.joda.time.Days
 import org.apache.spark._
-import com.johnsnowlabs.nlp.annotators.{Normalizer, Stemmer, Tokenizer}
-import com.johnsnowlabs.nlp.{DocumentAssembler,Finisher}
 import org.apache.spark.ml.feature.RegexTokenizer
 import org.apache.commons.lang3.StringUtils
 import scala.collection.mutable.WrappedArray
