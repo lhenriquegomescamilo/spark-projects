3c71596870998b02b1a43c42b689b5dda26db276
Martin Caravario <mcaravario@dc.uba.ar>
2020-03-30 18:31:45 -0300

    fix

--- Random/src/main/scala/RandomTincho.scala
+++ Random/src/main/scala/RandomTincho.scala
@@ -3327,7 +3327,7 @@ object RandomTincho {
       .map(day => path + "/day=%s/country=%s".format(day, country))
       .filter(path => fs.exists(new org.apache.hadoop.fs.Path(path)))
 
-    val udfString = udf((d: Int) => d.toString.substring(0, 4))
+    val udfString = udf((d: Int) => d.toString.take(5))
     // Get Data Raw
     spark.read
           .option("basePath", path)
