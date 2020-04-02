017dcacd142a09140370a5f7bc0a8dfe271f595c
matlongo <matlongo@gmail.com>
2020-04-02 15:12:05 -0300

    Getting BR users for demo audiences

--- Random/src/main/scala/Random.scala
+++ Random/src/main/scala/Random.scala
@@ -5087,7 +5087,7 @@ object Random {
     gender
       .unionAll(age)
       .withColumn("_c2", udfMap(col("_c2")))
-      .groupBy("_c0", "_c1")
+      // .groupBy("_c0", "_c1")
       // .agg(collect_set("_c2") as "_c2")
       // .withColumn("_c2", concat_ws(",", col("_c2")))
       .select("_c0", "_c1", "_c2")
